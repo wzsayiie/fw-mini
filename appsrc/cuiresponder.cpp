b667bbafaef62b6dcf139e45f4c1a1a9ea76b3df
wzsayiie <wzsayiie@qq.com>
2021-06-30 02:28:37 +0800

    added 'responder' frame.

--- appsrc/cuiresponder.cpp
+++ appsrc/cuiresponder.cpp
@@ -1,1 +1,31 @@
 #include "cuiresponder.h"
+
+CUIResponder::~CUIResponder() {
+}
+
+void CUIResponder::asRootResponder() {
+}
+
+void CUIResponder::becomeFirstResponder() {
+}
+
+void CUIResponder::resignFirstResponder() {
+}
+
+void CUIResponder::handleWindowEvent(MObject *, MObject *) {
+}
+
+void CUIResponder::handleTouchBegin() {
+}
+
+void CUIResponder::handleTouchMove() {
+}
+
+void CUIResponder::handleTouchEnd() {
+}
+
+void CUIResponder::handleKeyDown() {
+}
+
+void CUIResponder::handleText() {
+}
--- appsrc/cuiresponder.h
+++ appsrc/cuiresponder.h
@@ -1,8 +1,44 @@
 #pragma once
 
 #include <memory>
+#include <string>
+#include "mhostui.h"
 
 typedef std::shared_ptr<class CUIResponder> CUIResponderRef;
 
 class CUIResponder {
+    
+public:
+    virtual ~CUIResponder();
+    
+    void asRootResponder();
+    
+    virtual void becomeFirstResponder();
+    virtual void resignFirstResponder();
+    
+    virtual bool canRespondTouch() { return false; }
+    virtual bool canRespondText () { return false; }
+    virtual bool canRespondKey  () { return false; }
+    
+    virtual CUIResponder *findTouchResponder(float x, float y) { return nullptr; }
+    virtual CUIResponder *findTextResponder () { return nullptr; }
+    virtual CUIResponder *findKeyResponder  () { return nullptr; }
+    
+protected:
+    virtual void touchBegin(float x, float y) {}
+    virtual void touchMove (float x, float y) {}
+    virtual void touchEnd  (float x, float y) {}
+    
+    virtual void textInput(const std::string &string, bool enter) {}
+    
+    virtual void keyDown(MKey key) {}
+    
+private:
+    static void handleWindowEvent(MObject *, MObject *);
+    
+    void handleTouchBegin();
+    void handleTouchMove();
+    void handleTouchEnd();
+    void handleKeyDown();
+    void handleText();
 };
