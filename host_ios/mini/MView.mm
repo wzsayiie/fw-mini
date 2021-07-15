ceb87ee99f396a319bf89edfe283e2b648a785b6
wzsayiie <wzsayiie@qq.com>
2021-07-15 21:17:32 +0800

    added function for drawing string on macos and ios.

--- host_ios/mini/MView.mm
+++ host_ios/mini/MView.mm
@@ -6,36 +6,39 @@
 - (void)drawRect:(CGRect)rect {
     _MWindowOnDraw();
     
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
     int triangleCount = _MWindowTriangleCount();
     for (int index = 0; index < triangleCount; ++index) {
-        [self context:context drawTriangle:index];
+        [self drawTriangle:index];
     }
     
     int labelCount = _MWindowLabelCount();
     for (int index = 0; index < labelCount; ++index) {
-        [self context:context drawLabel:index];
+        [self drawLabel:index];
+    }
+    
+    int imageCount = _MWindowImageCount();
+    for (int index = 0; index < imageCount; ++index) {
+        [self drawImage:index];
     }
 }
 
-- (void)context:(CGContextRef)context setFillStrokeRGBA:(MColor)rgba {
-    auto color = (MColorPattern *)&rgba;
+- (void)drawTriangle:(int)index {
+    CGContextRef context = UIGraphicsGetCurrentContext();
     
-    float red   = color->red   / 255.f;
-    float green = color->green / 255.f;
-    float blue  = color->blue  / 255.f;
-    float alpha = color->alpha / 255.f;
+    //set the color:
+    MColorPattern color {
+        .rgba = _MWindowTriangleColor(index)
+    };
+    
+    float red   = color.red   / 255.f;
+    float green = color.green / 255.f;
+    float blue  = color.blue  / 255.f;
+    float alpha = color.alpha / 255.f;
     
     CGContextSetRGBFillColor  (context, red, green, blue, alpha);
     CGContextSetRGBStrokeColor(context, red, green, blue, alpha);
-}
-
-- (void)context:(CGContextRef)context drawTriangle:(int)index {
-    MColor rgba = _MWindowTriangleColor(index);
-    [self context:context setFillStrokeRGBA:rgba];
-    CGContextSetLineWidth(context, 1);
     
+    //connect the points:
     float x0 = _MWindowTriangleVertexX(index, 0);
     float y0 = _MWindowTriangleVertexY(index, 0);
     float x1 = _MWindowTriangleVertexX(index, 1);
@@ -47,10 +50,56 @@
     CGContextAddLineToPoint(context, x1, y1);
     CGContextAddLineToPoint(context, x2, y2);
     
+    //draw.
     CGContextDrawPath(context, kCGPathFillStroke);
 }
 
-- (void)context:(CGContextRef)context drawLabel:(int)index {
+- (void)drawLabel:(int)index {
+    //set the color:
+    MColorPattern nativeColor = {
+        .rgba = _MWindowLabelColor(index)
+    };
+    
+    float red   = nativeColor.red   / 255.f;
+    float green = nativeColor.green / 255.f;
+    float blue  = nativeColor.blue  / 255.f;
+    float alpha = nativeColor.alpha / 255.f;
+    
+    UIColor *color = [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+    
+    //set the font.
+    float fontSize = _MWindowLabelFontSize(index);
+    UIFont *font = [UIFont systemFontOfSize:fontSize];
+    
+    //calculate the position:
+    MString *nativeString = _MWindowLabelString(index);
+    NSString *string = @(MStringU8Chars(nativeString));
+    
+    NSDictionary *attributes = @{
+        NSForegroundColorAttributeName: color,
+        NSFontAttributeName: font,
+    };
+    CGSize size = [string sizeWithAttributes:attributes];
+    
+    float X = _MWindowLabelX(index);
+    float Y = _MWindowLabelY(index);
+    float W = _MWindowLabelWidth (index);
+    float H = _MWindowLabelHeight(index);
+    
+    CGPoint origin = CGPointZero;
+    MAligns aligns = _MWindowLabelAligns(index);
+    if /**/ (aligns & MAlign_Left   ) { origin.x = X; }
+    else if (aligns & MAlign_HCenter) { origin.x = X + (W - size.width ) / 2; }
+    else if (aligns & MAlign_Right  ) { origin.x = X + (W - size.width ); }
+    if /**/ (aligns & MAlign_Top    ) { origin.y = Y; }
+    else if (aligns & MAlign_VCenter) { origin.y = Y + (H - size.height) / 2; }
+    else if (aligns & MAlign_Bottom ) { origin.y = Y + (H - size.height); }
+    
+    //draw.
+    [string drawAtPoint:origin withAttributes:attributes];
+}
+
+- (void)drawImage:(int)index {
 }
 
 @end
--- host_osx/mini/MView.mm
+++ host_osx/mini/MView.mm
@@ -6,37 +6,41 @@
 - (void)drawRect:(NSRect)dirtyRect {
     _MWindowOnDraw();
     
-    CGContextRef context = NSGraphicsContext.currentContext.CGContext;
     float height = self.frame.size.height;
 
     int triangleCount = _MWindowTriangleCount();
     for (int index = 0; index < triangleCount; ++index) {
-        [self context:context drawTriangle:index viewHeight:height];
+        [self drawTriangle:index withViewHeight:height];
     }
 
     int labelCount = _MWindowLabelCount();
     for (int index = 0; index < labelCount; ++index) {
-        [self context:context drawLabel:index viewHeight:height];
+        [self drawLabel:index withViewHeight:height];
+    }
+    
+    int imageCount = _MWindowImageCount();
+    for (int index = 0; index < imageCount; ++index) {
+        [self drawImage:index withViewHeight:height];
     }
 }
 
-- (void)context:(CGContextRef)context setFillStrokeRGBA:(MColor)rgba {
-    auto color = (MColorPattern *)&rgba;
+- (void)drawTriangle:(int)index withViewHeight:(float)viewHeight {
+    CGContextRef context = NSGraphicsContext.currentContext.CGContext;
     
-    float red   = color->red   / 255.f;
-    float green = color->green / 255.f;
-    float blue  = color->blue  / 255.f;
-    float alpha = color->alpha / 255.f;
+    //set the color:
+    MColorPattern color = {
+        .rgba = _MWindowTriangleColor(index)
+    };
+    
+    float red   = color.red   / 255.f;
+    float green = color.green / 255.f;
+    float blue  = color.blue  / 255.f;
+    float alpha = color.alpha / 255.f;
     
     CGContextSetRGBFillColor  (context, red, green, blue, alpha);
     CGContextSetRGBStrokeColor(context, red, green, blue, alpha);
-}
-
-- (void)context:(CGContextRef)context drawTriangle:(int)index viewHeight:(float)height {
-    MColor rgba = _MWindowTriangleColor(index);
-    [self context:context setFillStrokeRGBA:rgba];
-    CGContextSetLineWidth(context, 1);
     
+    //connect the points:
     float x0 = _MWindowTriangleVertexX(index, 0);
     float y0 = _MWindowTriangleVertexY(index, 0);
     float x1 = _MWindowTriangleVertexX(index, 1);
@@ -44,14 +48,60 @@
     float x2 = _MWindowTriangleVertexX(index, 2);
     float y2 = _MWindowTriangleVertexY(index, 2);
     
-    CGContextMoveToPoint   (context, x0, height - y0);
-    CGContextAddLineToPoint(context, x1, height - y1);
-    CGContextAddLineToPoint(context, x2, height - y2);
+    CGContextMoveToPoint   (context, x0, viewHeight - y0);
+    CGContextAddLineToPoint(context, x1, viewHeight - y1);
+    CGContextAddLineToPoint(context, x2, viewHeight - y2);
     
+    //draw.
     CGContextDrawPath(context, kCGPathFillStroke);
 }
 
-- (void)context:(CGContextRef)context drawLabel:(int)index viewHeight:(float)height {
+- (void)drawLabel:(int)index withViewHeight:(float)viewHeight {
+    //set the color:
+    MColorPattern nativeColor = {
+        .rgba = _MWindowLabelColor(index)
+    };
+    
+    float red   = nativeColor.red   / 255.f;
+    float green = nativeColor.green / 255.f;
+    float blue  = nativeColor.blue  / 255.f;
+    float alpha = nativeColor.alpha / 255.f;
+    
+    NSColor *color = [NSColor colorWithRed:red green:green blue:blue alpha:alpha];
+    
+    //set the font.
+    float fontSize = _MWindowLabelFontSize(index);
+    NSFont *font = [NSFont systemFontOfSize:fontSize];
+    
+    //calculate the position:
+    MString *nativeString = _MWindowLabelString(index);
+    NSString *string = @(MStringU8Chars(nativeString));
+    
+    NSDictionary *attributes = @{
+        NSForegroundColorAttributeName: color,
+        NSFontAttributeName: font,
+    };
+    NSSize size = [string sizeWithAttributes:attributes];
+    
+    float W = _MWindowLabelWidth (index);
+    float H = _MWindowLabelHeight(index);
+    float X = _MWindowLabelX(index);
+    float Y = viewHeight - H - _MWindowLabelY(index);
+    
+    NSPoint origin = NSZeroPoint;
+    MAligns aligns = _MWindowLabelAligns(index);
+    if /**/ (aligns & MAlign_Left   ) { origin.x = X; }
+    else if (aligns & MAlign_HCenter) { origin.x = X + (W - size.width ) / 2; }
+    else if (aligns & MAlign_Right  ) { origin.x = X + (W - size.width ); }
+    if /**/ (aligns & MAlign_Top    ) { origin.y = Y + (H - size.height); }
+    else if (aligns & MAlign_VCenter) { origin.y = Y + (H - size.height) / 2; }
+    else if (aligns & MAlign_Bottom ) { origin.y = Y; }
+    
+    //draw.
+    [string drawAtPoint:origin withAttributes:attributes];
+}
+
+- (void)drawImage:(int)index withViewHeight:(float)viewHeight {
 }
 
 @end
