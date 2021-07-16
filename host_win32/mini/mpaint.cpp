a7c04d0817812411d84ad10cadca0674642c929a
wujia <wujia@dianchu.com>
2021-07-16 17:38:53 +0800

    used 'wchar' replace 'char16_t' in win32 api.

--- host_win32/mini/mpaint.cpp
+++ host_win32/mini/mpaint.cpp
@@ -58,7 +58,7 @@ static void PaintLabel(Gdiplus::Graphics *graphics, int index)
 {
     //get the string.
     MString *string = _MWindowLabelString(index);
-    auto    *wchars = (const wchar_t *)MStringU16Chars(string);
+    auto    *wchars = (const WCHAR *)MStringU16Chars(string);
     int      wsize  = MStringU16Size(string);
 
     //set the font.
