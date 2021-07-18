ae63abdfc9266092d654196469defe3114c3a8d4
wzsayiie <wzsayiie@qq.com>
2021-07-18 20:18:19 +0800

    split the declaration and definition of basic types.

--- appsrc/mtypes.cpp
+++ appsrc/mtypes.cpp
@@ -1,26 +1,282 @@
 #include "mtypes.h"
 
-MArray::MArray() : MObject(MType_MArray) {
+//------------------------------------------------------------------------------
+//MObject:
+
+void MObject::_retain() {
+    mRefCount += 1;
 }
 
-MArray::~MArray() {
-    for (MObject *item : mItems) {
-        MRelease(item);
+void MObject::_release() {
+    if (--mRefCount == 0) {
+        delete this;
     }
 }
 
-void MArray::_append(MObject *item) {
-    MRetain(item);
-    mItems.push_back(item);
+void MRetain(MObject *object) {
+    if (object) {
+        object->_retain();
+    }
 }
 
-int MArray::_length() {
-    return (int)mItems.size();
+void MRelease(MObject *object) {
+    if (object) {
+        object->_release();
+    }
 }
 
-MObject *MArray::_item(int index) {
-    if (0 <= index && index < _length()) {
-        return mItems[index];
+MType MGetType(MObject *object) {
+    if (object) {
+        return object->_type();
+    }
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+//MBool & MInt & MFloat:
+
+template<typename T, int ID, typename BASE> struct _MNumber : BASE {
+
+    T value;
+    
+    _MNumber(T value) {
+        this->value = value;
+    }
+
+    MType _type() override {
+        return ID;
+    }
+};
+
+typedef _MNumber<bool , MType_Bool , MBool > MBoolImpl ;
+typedef _MNumber<int  , MType_Int  , MInt  > MIntImpl  ;
+typedef _MNumber<float, MType_Float, MFloat> MFloatImpl;
+
+MBool  *MBoolCreate (bool  value) { return new MBoolImpl (value); }
+MInt   *MIntCreate  (int   value) { return new MIntImpl  (value); }
+MFloat *MFloatCreate(float value) { return new MFloatImpl(value); }
+
+bool  MBoolValue (MBool  *obj) { return obj ? ((MBoolImpl  *)obj)->value : 0; }
+int   MIntValue  (MInt   *obj) { return obj ? ((MIntImpl   *)obj)->value : 0; }
+float MFloatValue(MFloat *obj) { return obj ? ((MFloatImpl *)obj)->value : 0; }
+
+//------------------------------------------------------------------------------
+//MString:
+
+struct MStringImpl : MString {
+
+    std::string    u8string ;
+    std::u16string u16string;
+    
+    MStringImpl(const char *chars) {
+        if (chars) {
+            u8string = chars;
+        }
+    }
+
+    MStringImpl(const char16_t *chars) {
+        if (chars) {
+            u16string = chars;
+        }
+    }
+    
+    const std::string &u8s() {
+        if (u8string.empty() && !u16string.empty()) {
+            u8string = MU8StringFromU16(u16string.c_str());
+        }
+        return u8string;
+    }
+
+    const std::u16string &u16s() {
+        if (u16string.empty() && !u8string.empty()) {
+            u16string = MU16StringFromU8(u8string.c_str());
+        }
+        return u16string;
+    }
+    
+    MType _type() override {
+        return MType_MString;
+    }
+};
+
+MString *MStringCreateU8 (const char     *chs) { return chs ? new MStringImpl(chs) : nullptr; }
+MString *MStringCreateU16(const char16_t *chs) { return chs ? new MStringImpl(chs) : nullptr; }
+
+const char     *MStringU8Chars (MString *str) { return str ? ((MStringImpl *)str)->u8s ().c_str() : nullptr; }
+const char16_t *MStringU16Chars(MString *str) { return str ? ((MStringImpl *)str)->u16s().c_str() : nullptr; }
+
+int MStringU8Size (MString *str) { return str ? (int)((MStringImpl *)str)->u8s ().size() : 0; }
+int MStringU16Size(MString *str) { return str ? (int)((MStringImpl *)str)->u16s().size() : 0; }
+
+//------------------------------------------------------------------------------
+//MLambda:
+
+struct MLambdaImpl : MLambda {
+    
+    MLambdaFunc mFunc;
+    MObject    *mLoad;
+    
+    MLambdaImpl(MLambdaFunc func, MObject *load) {
+        MRetain(load);
+        mFunc = func;
+        mLoad = load;
+    }
+
+    ~MLambdaImpl() {
+        MRelease(mLoad);
+    }
+    
+    MType _type() override {
+        return MType_MLambda;
+    }
+};
+
+MLambda *MLambdaCreate(MLambdaFunc func, MObject *load) {
+    if (func) {
+        return new MLambdaImpl(func, load);
     }
     return nullptr;
+}
+
+void MLambdaCall(MLambda *lambda, MObject *param) {
+    if (lambda) {
+        auto object = (MLambdaImpl *)lambda;
+        object->mFunc(object->mLoad, param);
+    }
+}
+
+//------------------------------------------------------------------------------
+//MData:
+
+struct MDataImpl : MData {
+    
+    std::vector<uint8_t> bytes;
+    
+    MType _type() override {
+        return MType_MData;
+    }
+};
+
+MData *MDataCreate(const uint8_t *bytes, int size) {
+    if (!bytes && size <= 0) {
+        return nullptr;
+    }
+    
+    auto data = new MDataImpl();
+    data->bytes.insert(data->bytes.end(), bytes, bytes + size);
+    return data;
+}
+
+void MDataAppend(MData *data, const uint8_t *bytes, int size) {
+    if (data && bytes && size > 0) {
+        auto object = (MDataImpl *)data;
+        object->bytes.insert(object->bytes.end(), bytes, bytes + size);
+    }
+}
+
+int MDataSize(MData *data) {
+    if (data) {
+        auto object = (MDataImpl *)data;
+        return (int)object->bytes.size();
+    }
+    return 0;
+}
+
+const uint8_t *MDataBytes(MData *data) {
+    if (data) {
+        auto object = (MDataImpl *)data;
+        return &object->bytes[0];
+    }
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+//MArray:
+
+struct MArrayImpl : MArray {
+    
+    std::vector<MObject *> items;
+    
+    ~MArrayImpl() {
+        for (MObject *it : items) {
+            MRelease(it);
+        }
+    }
+
+    MType _type() override {
+        return MType_MArray;
+    }
+};
+
+MArray *MArrayCreate() {
+    return new MArrayImpl();
+}
+
+void MArrayAppend(MArray *array, MObject *item) {
+    if (!array) {
+        return;
+    }
+    
+    MRetain(item);
+    
+    auto object = (MArrayImpl *)array;
+    object->items.push_back(item);
+}
+
+int MArrayLength(MArray *array) {
+    if (array) {
+        auto object = (MArrayImpl *)array;
+        return (int)object->items.size();
+    }
+    return 0;
+}
+
+MObject *MArrayItem(MArray *array, int index) {
+    auto object = (MArrayImpl *)array;
+    if (!object) {
+        return nullptr;
+    }
+    
+    if (index < 0 || (int)object->items.size() <= index) {
+        return nullptr;
+    }
+    
+    return object->items[index];
+}
+
+//------------------------------------------------------------------------------
+//MImage:
+
+struct MImageImpl : MImage {
+    
+    int managedId = 0;
+    MImageDispose dispose = nullptr;
+
+    MImageImpl(int managedId, MImageDispose dispose) {
+        this->managedId = managedId;
+        this->dispose = dispose;
+    }
+    
+    ~MImageImpl() {
+        dispose(managedId);
+    }
+    
+    MType _type() override {
+        return MType_MImage;
+    }
+};
+
+MImage *MImageCreate(int managedId, MImageDispose dispose) {
+    if (managedId != 0) {
+        return new MImageImpl(managedId, dispose);
+    }
+    return nullptr;
+}
+
+int MImageManagedId(MImage *image) {
+    if (image) {
+        auto object = (MImageImpl *)image;
+        return object->managedId;
+    }
+    return 0;
 }
--- appsrc/mtypes.h
+++ appsrc/mtypes.h
@@ -34,281 +34,88 @@ const MType MType_MImage  = MEnumId("Img");     //MImage.
 class MObject {
 
 public:
-    void _retain() {
-        mRefCount += 1;
-    }
-
-    void _release() {
-        if (--mRefCount == 0) {
-            delete this;
-        }
-    }
-
-    MType _type() {
-        return mType;
-    }
+    void _retain ();
+    void _release();
+    
+    virtual MType _type() = 0;
 
 protected:
-    MObject(MType type) {
-        mRefCount = 1;
-        mType = type;
-    }
-
-    virtual ~MObject() {
-    }
+    virtual ~MObject() {}
 
 private:
-    int   mRefCount;
-    MType mType;
+    int mRefCount = 1;
 };
 
-extern "C" inline void MRetain(MObject *object) {
-    if (object) {
-        object->_retain();
-    }
-}
-
-extern "C" inline void MRelease(MObject *object) {
-    if (object) {
-        object->_release();
-    }
-}
-
-extern "C" inline MType MGetType(MObject *object) {
-    return object ? object->_type() : 0;
-}
+extern "C" void  MRetain (MObject *object);
+extern "C" void  MRelease(MObject *object);
+extern "C" MType MGetType(MObject *object);
 
 //------------------------------------------------------------------------------
 //MBool & MInt & MFloat:
 
-template<typename T, int ID> class _MNumber : public MObject {
+class MBool : public MObject {};
+class MInt  : public MObject {};
+class MFloat: public MObject {};
 
-public:
-    _MNumber(T value) : MObject(ID) {
-        mValue = value;
-    }
+extern "C" MBool  *MBoolCreate (bool  value);
+extern "C" MInt   *MIntCreate  (int   value);
+extern "C" MFloat *MFloatCreate(float value);
 
-    T _value() {
-        return mValue;
-    }
-
-private:
-    T mValue;
-};
-
-typedef _MNumber<bool , MType_Bool > MBool ;
-typedef _MNumber<int  , MType_Int  > MInt  ;
-typedef _MNumber<float, MType_Float> MFloat;
-
-extern "C" inline MBool  *MBoolCreate (bool  v) { return new MBool (v); }
-extern "C" inline MInt   *MIntCreate  (int   v) { return new MInt  (v); }
-extern "C" inline MFloat *MFloatCreate(float v) { return new MFloat(v); }
-
-extern "C" inline bool  MBoolValue (MBool  *a) { return a ? a->_value() : 0; }
-extern "C" inline int   MIntValue  (MInt   *a) { return a ? a->_value() : 0; }
-extern "C" inline float MFloatValue(MFloat *a) { return a ? a->_value() : 0; }
+extern "C" bool  MBoolValue (MBool  *object);
+extern "C" int   MIntValue  (MInt   *object);
+extern "C" float MFloatValue(MFloat *object);
 
 //------------------------------------------------------------------------------
 //MString:
 
-class MString : public MObject {
+class MString : public MObject {};
 
-public:
-    MString(const char *chars) : MObject(MType_MString) {
-        if (chars) {
-            mU8String = chars;
-        }
-    }
+extern "C" MString *MStringCreateU8 (const char     *chars);
+extern "C" MString *MStringCreateU16(const char16_t *chars);
 
-    MString(const char16_t *chars) : MObject(MType_MString) {
-        if (chars) {
-            mU16String = chars;
-        }
-    }
+extern "C" const char     *MStringU8Chars (MString *string);
+extern "C" const char16_t *MStringU16Chars(MString *string);
 
-    const char     *_u8chars () { return u8string ().c_str(); }
-    const char16_t *_u16chars() { return u16string().c_str(); }
-
-    int _u8size () { return (int)u8string ().size(); }
-    int _u16size() { return (int)u16string().size(); }
-    
-private:
-    const std::string &u8string() {
-        if (mU8String.empty() && !mU16String.empty()) {
-            mU8String = MU8StringFromU16(mU16String.c_str());
-        }
-        return mU8String;
-    }
-
-    const std::u16string &u16string() {
-        if (mU16String.empty() && !mU8String.empty()) {
-            mU16String = MU16StringFromU8(mU8String.c_str());
-        }
-        return mU16String;
-    }
-
-    std::string    mU8String ;
-    std::u16string mU16String;
-};
-
-extern "C" inline MString *MStringCreateU8 (const char     *s) { return new MString(s); }
-extern "C" inline MString *MStringCreateU16(const char16_t *s) { return new MString(s); }
-
-extern "C" inline const char     *MStringU8Chars (MString *s) { return s ? s->_u8chars () : nullptr; }
-extern "C" inline const char16_t *MStringU16Chars(MString *s) { return s ? s->_u16chars() : nullptr; }
-
-extern "C" inline int MStringU8Size (MString *s) { return s ? s->_u8size () : 0; }
-extern "C" inline int MStringU16Size(MString *s) { return s ? s->_u16size() : 0; }
+extern "C" int MStringU8Size (MString *string);
+extern "C" int MStringU16Size(MString *string);
 
 //------------------------------------------------------------------------------
 //MLambda:
 
 typedef void (*MLambdaFunc)(MObject *load, MObject *param);
 
-class MLambda : public MObject {
-    
-public:
-    MLambda(MLambdaFunc func, MObject *load) : MObject(MType_MLambda) {
-        MRetain(load);
+class MLambda : public MObject {};
 
-        mFunc = func;
-        mLoad = load;
-    }
-
-    ~MLambda() {
-        MRelease(mLoad);
-    }
-
-    void _call(MObject *param) {
-        mFunc(mLoad, param);
-    }
-
-private:
-    MLambdaFunc mFunc;
-    MObject *mLoad;
-};
-
-extern "C" inline MLambda *MLambdaCreate(MLambdaFunc func, MObject *load) {
-    return new MLambda(func, load);
-}
-
-extern "C" inline void MLambdaCall(MLambda *lambda, MObject *param) {
-    if (lambda) {
-        lambda->_call(param);
-    }
-}
+extern "C" MLambda *MLambdaCreate(MLambdaFunc func, MObject *load);
+extern "C" void MLambdaCall(MLambda *lambda, MObject *param);
 
 //------------------------------------------------------------------------------
 //MData:
 
-class MData : public MObject {
-    
-public:
-    MData(const uint8_t *bytes, int size) : MObject(MType_MData) {
-        _append(bytes, size);
-    }
+class MData : public MObject {};
 
-    void _append(const uint8_t *bytes, int size) {
-        if (bytes && size > 0) {
-            mBytes.insert(mBytes.end(), bytes, bytes + size);
-        }
-    }
+extern "C" MData *MDataCreate(const uint8_t *bytes, int size);
+extern "C" void   MDataAppend(MData *data, const uint8_t *bytes, int size);
+extern "C" int    MDataSize  (MData *data);
 
-    const uint8_t *_bytes() {
-        return &mBytes[0];
-    }
-
-    int _size() {
-        return (int)mBytes.size();
-    }
-
-private:
-    std::vector<uint8_t> mBytes;
-};
-
-extern "C" inline MData *MDataCreate(const uint8_t *bytes, int size) {
-    return new MData(bytes, size);
-}
-
-extern "C" inline void MDataAppend(MData *data, const uint8_t *bytes, int size) {
-    if (data) {
-        data->_append(bytes, size);
-    }
-}
-
-extern "C" inline const uint8_t *MDataBytes(MData *data) {
-    return data ? data->_bytes() : nullptr;
-}
-
-extern "C" inline int MDataSize(MData *data) {
-    return data ? data->_size() : 0;
-}
+extern "C" const uint8_t *MDataBytes(MData *data);
 
 //------------------------------------------------------------------------------
 //MArray:
 
-class MArray : public MObject {
-    
-public:
-    MArray();
-    ~MArray();
+class MArray : public MObject {};
 
-    void _append(MObject *item);
-    int _length();
-    MObject *_item(int index);
-
-private:
-    std::vector<MObject *> mItems;
-};
-
-extern "C" inline MArray *MArrayCreate() {
-    return new MArray();
-}
-
-extern "C" inline void MArrayAppend(MArray *array, MObject *item) {
-    if (array) {
-        array->_append(item);
-    }
-}
-
-extern "C" inline int MArrayLength(MArray *array) {
-    return array ? array->_length() : 0;
-}
-
-extern "C" inline MObject *MArrayItem(MArray *array, int index) {
-    return array ? array->_item(index) : 0;
-}
+extern "C" MArray  *MArrayCreate();
+extern "C" void     MArrayAppend(MArray *array, MObject *item);
+extern "C" int      MArrayLength(MArray *array);
+extern "C" MObject *MArrayItem  (MArray *array, int index);
 
 //------------------------------------------------------------------------------
 //MImage:
 
 typedef void (*MImageDispose)(int managedId);
 
-class MImage : public MObject {
-    
-public:
-    MImage(int managedId, MImageDispose dispose) : MObject(MType_MImage) {
-        mManagedId = managedId;
-        mDispose = dispose;
-    }
+class MImage : public MObject {};
 
-    ~MImage() {
-        mDispose(mManagedId);
-    }
-
-    int _managedId() {
-        return mManagedId;
-    }
-
-private:
-    int mManagedId = 0;
-    MImageDispose mDispose = nullptr;
-};
-
-extern "C" inline MImage *MImageCreate(int managedId, MImageDispose dispose) {
-    return managedId != 0 ? new MImage(managedId, dispose) : nullptr;
-}
-
-extern "C" inline int MImageManagedId(MImage *image) {
-    return image ? image->_managedId() : 0;
-}
+extern "C" MImage *MImageCreate(int managedId, MImageDispose dispose);
+extern "C" int MImageManagedId(MImage *image);
