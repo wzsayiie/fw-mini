174e8d3ea419c4cb2927f226104493b4cb37f932
wzsayiie <wzsayiie@qq.com>
2021-07-25 19:35:21 +0800

    added 'maincontroller'.

--- appsrc/capp.cpp
+++ appsrc/capp.cpp
@@ -1,4 +1,12 @@
 #include "cuikit.h"
 
+class MainController : public CViewController {
+
+protected:
+    void onViewLoad() override {
+    }
+};
+
 MAPP_UI_LAUNCH(CAppLaunch) void CAppLaunch() {
+    (new MainController)->asRootController();
 }
