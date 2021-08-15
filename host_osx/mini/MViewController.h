da68d7db719c4735e3b78a7df5330bb39696feb9
wzsayiie <wzsayiie@qq.com>
2021-08-15 23:19:45 +0800

    added text input support on mac os.

--- host_osx/mini/MViewController.h
+++ host_osx/mini/MViewController.h
@@ -1,4 +1,6 @@
 #import <Cocoa/Cocoa.h>
 
-@interface MViewController : NSViewController
+@interface MViewController : NSViewController <NSTextFieldDelegate>
+@property (nonatomic) NSTextField *textField;
+@property (nonatomic) BOOL textFieldEditing;
 @end
--- host_osx/mini/MViewController.mm
+++ host_osx/mini/MViewController.mm
@@ -16,20 +16,23 @@
     
     MRegisterAPIs();
     
-    //application events.
+    NSSize size = self.view.frame.size;
+    [self adjustedTextFieldFrameWithSuperSize:size];
+    
+    //application events:
     _MAppLaunch();
+    
     [NSTimer scheduledTimerWithTimeInterval:_MAppUpdateInterval repeats:YES block:^(NSTimer *timer) {
+        [self updateTextFieldState];
         _MAppUpdate();
     }];
     
     //window events:
-    NSSize size = self.view.frame.size;
     _MWindowOnResize(size.width, size.height);
     _MWindowOnLoad();
     
-    __weak NSView *weakView = self.view;
     [NSTimer scheduledTimerWithTimeInterval:_MWindowDrawInterval repeats:YES block:^(NSTimer *timer) {
-        [weakView setNeedsDisplay:YES];
+        [self.view setNeedsDisplay:YES];
     }];
     
     NSNotificationCenter *center = NSNotificationCenter.defaultCenter;
@@ -37,10 +40,9 @@
     [center addObserver:self selector:@selector(windowDidMiniaturize) name:NSWindowDidMiniaturizeNotification object:nil];
     [center addObserver:self selector:@selector(windowDidResize)      name:NSWindowDidResizeNotification      object:nil];
     
-    __weak MViewController *weakController = self;
+    //NOTE: the monitor will intercept ALL key down events.
     [NSEvent addLocalMonitorForEventsMatchingMask:NSEventMaskKeyDown handler:^NSEvent *(NSEvent *event) {
-        [weakController keyboardKeyDown:event];
-        return nil;
+        return [self keyboardKeyDown:event];
     }];
 }
 
@@ -54,6 +56,7 @@
 
 - (void)windowDidResize {
     NSSize size = self.view.frame.size;
+    [self adjustedTextFieldFrameWithSuperSize:size];
     _MWindowOnResize(size.width, size.height);
 }
 
@@ -66,25 +69,93 @@
 - (void)mouseDragged:(NSEvent *)event { [self mouseEvent:event func:_MWindowOnTouchMove ]; }
 - (void)mouseUp     :(NSEvent *)event { [self mouseEvent:event func:_MWindowOnTouchEnd  ]; }
 
-- (void)keyboardKeyDown:(NSEvent *)event {
-    if (event.modifierFlags & NSEventModifierFlagDeviceIndependentFlagsMask) {
+- (NSTextField *)textField {
+    if (!_textField) {
+        _textField = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 300, 20)];
+        _textField.delegate = self;
+        _textField.hidden = YES;
+        
+        [self.view addSubview:_textField];
+    }
+    return _textField;
+}
+
+- (void)adjustedTextFieldFrameWithSuperSize:(NSSize)superSize {
+    NSRect frame = self.textField.frame;
+    
+    frame.origin.x = (superSize.width - frame.size.width) / 2;
+    frame.origin.y = 20;
+    
+    self.textField.frame = frame;
+}
+
+- (void)updateTextFieldState {
+    if (!_MWindowTextBoxUpdated()) {
         return;
     }
     
-    switch (event.keyCode) {
-        case  51: _MWindowOnKeyDown(MKey_Back ); break;
-        case  36: _MWindowOnKeyDown(MKey_Enter); break;
-        case  49: _MWindowOnKeyDown(MKey_Space); break;
-        case 123: _MWindowOnKeyDown(MKey_Left ); break;
-        case 126: _MWindowOnKeyDown(MKey_Up   ); break;
-        case 124: _MWindowOnKeyDown(MKey_Right); break;
-        case 125: _MWindowOnKeyDown(MKey_Down ); break;
-        case   0: _MWindowOnKeyDown(MKey_A    ); break;
-        case  13: _MWindowOnKeyDown(MKey_W    ); break;
-        case   2: _MWindowOnKeyDown(MKey_D    ); break;
-        case   1: _MWindowOnKeyDown(MKey_S    ); break;
+    //IMPORTANT: reset the flag;
+    MWindowSetTextBoxUpdated(false);
+    
+    if (_MWindowTextBoxEnabled()) {
+        self.textFieldEditing = YES;
+        
+        MString  *raw  = _MWindowTextBoxRawString();
+        NSString *text = [NSString stringWithUTF8String:MStringU8Chars(raw)];
+        
+        [self.textField setStringValue:text];
+        [self.textField setHidden:NO];
+        [self.textField becomeFirstResponder];
+        
+    } else {
+        self.textFieldEditing = NO;
+        
+        [self.textField resignFirstResponder];
+        [self.textField setHidden:YES];
+    }
+}
 
-        default:;
+- (void)makeTextBoxEventWithEnter:(bool)enter {
+    NSString  *value  = self.textField.stringValue;
+    MStringRef string = m_auto_release MStringCreateU8(value.UTF8String);
+    
+    _MWindowOnTextBox(string.get(), enter);
+}
+
+- (void)controlTextDidChange:(NSNotification *)notification {
+    [self makeTextBoxEventWithEnter:false];
+}
+
+- (NSEvent *)keyboardKeyDown:(NSEvent *)event {
+    //do not process keys with modifier keys.
+    if (event.modifierFlags & NSEventModifierFlagDeviceIndependentFlagsMask) {
+        return event;
+    }
+    
+    if (self.textFieldEditing) {
+        if (event.keyCode == 36 /* enter */) {
+            [self makeTextBoxEventWithEnter:true];
+            return nil;
+        } else {
+            //pass the event to the textfield.
+            return event;
+        }
+    } else {
+        switch (event.keyCode) {
+            case  51: _MWindowOnKeyDown(MKey_Back ); break;
+            case  36: _MWindowOnKeyDown(MKey_Enter); break;
+            case  49: _MWindowOnKeyDown(MKey_Space); break;
+            case 123: _MWindowOnKeyDown(MKey_Left ); break;
+            case 126: _MWindowOnKeyDown(MKey_Up   ); break;
+            case 124: _MWindowOnKeyDown(MKey_Right); break;
+            case 125: _MWindowOnKeyDown(MKey_Down ); break;
+            case   0: _MWindowOnKeyDown(MKey_A    ); break;
+            case  13: _MWindowOnKeyDown(MKey_W    ); break;
+            case   2: _MWindowOnKeyDown(MKey_D    ); break;
+            case   1: _MWindowOnKeyDown(MKey_S    ); break;
+            default :;
+        }
+        return nil;
     }
 }
 
