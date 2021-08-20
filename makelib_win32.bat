aa8fe49028d61e05a718ca24f3046b7329ba124a
wujia <wzsayiie@qq.com>
2021-08-20 19:17:36 +0800

    adjusetd coding style.

--- makelib_win32.bat
+++ makelib_win32.bat
@@ -2,20 +2,29 @@
 
 cd %~dp0
 
-set TMPDIR="MSCL"
-set OUTDIR="..\BUILD"
-set OUTEXE="..\BUILD\mini.dll"
-set INCDIR="..\appsrc"
-set CPPSRC="..\appsrc\m*.cpp" "..\host_win32\mini\*.cpp"
-set WINLIB="gdi32.lib" "gdiplus.lib" "ole32.lib" "shell32.lib" "user32.lib"
+::configuration begin.
+set _IncDir=appsrc
+set _CxxSrc=appsrc\m*.cpp host_win32\mini\*.cpp
+set _SysLib=gdi32.lib gdiplus.lib ole32.lib shell32.lib user32.lib
+set _CxxFlg=/nologo /std:c++14 /Ox /EHsc /LD
 
-mkdir %TMPDIR%
-pushd %TMPDIR%
+set _OutDir=BUILD
+set _OutDll=mini.dll
+set _OutLib=mini.lib
+set _OutExp=mini.exp
 
-if not exist %OUTDIR% (md  %OUTDIR%)
-if exist     %OUTEXE% (del %OUTEXE%)
+set _ObjDir=CL_TMP_OBJ
+::configuration end.
 
-cl /nologo /std:c++14 /EHsc /LD /Fe%OUTEXE% /I%INCDIR% %CPPSRC% %WINLIB%
+if not exist %_OutDir% (md %_OutDir%)
+if not exist %_ObjDir% (md %_ObjDir%)
 
+pushd %_OutDir%
+    if exist %_OutDll% (del %_OutDll%)
+    if exist %_OutLib% (del %_OutLib%)
+    if exist %_OutExp% (del %_OutExp%)
 popd
-rmdir /s /q %TMPDIR%
+
+cl /Fe"%_OutDir%\\%_OutDll%" /Fo"%_ObjDir%\\" /I%_IncDir% %_CxxFlg% %_CxxSrc% %_SysLib%
+
+rmdir /s /q %_ObjDir%
