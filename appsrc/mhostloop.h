3416d6e2f999da15aaac5db776eb867b1abe07e6
wujia <wzsayiie@qq.com>
2021-09-14 15:29:20 +0800

    adjusted draw priority.

--- appsrc/mhostloop.h
+++ appsrc/mhostloop.h
@@ -13,8 +13,8 @@ MFUNC_EXPORT void _MAppAddUpdateListener(MLambda *listener);
 struct _MAppLaunchAdder { _MAppLaunchAdder(void (*func)(), int priority); };
 struct _MAppUpdateAdder { _MAppUpdateAdder(void (*func)()); };
 
-const int MAppLaunchPriority_UI    = 2000;
-const int MAppLaunchPriority_Scene = 1000;
+const int MAppLaunchPriority_Scene = 2000;
+const int MAppLaunchPriority_UI    = 1000;
 
 #define MAPP_LAUNCH(func, priority) ; static _MAppLaunchAdder _launch_##func(func, priority); void func()
 #define MAPP_UPDATE(func)           ; static _MAppUpdateAdder _update_##func(func)          ; void func()
