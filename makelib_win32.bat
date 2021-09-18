0ce39e428a9e2669e57047aa35a8e2f0b85bac16
wujia <wzsayiie@qq.com>
2021-09-18 18:12:27 +0800

    do not retain 'exp'  and 'lib' files.

--- makelib_win32.bat
+++ makelib_win32.bat
@@ -1,30 +1,35 @@
 @echo off
 
+setlocal
+
 cd %~dp0
 
 ::configuration begin.
 set _IncDir=appsrc
 set _CxxSrc=appsrc\m*.cpp host_win32\mini\*.cpp
 set _SysLib=chakrart.lib gdi32.lib gdiplus.lib ole32.lib shell32.lib user32.lib
-set _CxxFlg=/nologo /std:c++14 /Ox /EHsc /LD
+set _CxxFlg=/nologo /std:c++14 /EHsc /Ox /MD /LD
 
 set _OutDir=BUILD
-set _OutDll=mini.dll
-set _OutLib=mini.lib
-set _OutExp=mini.exp
+set _OutNam=mini
 
-set _ObjDir=CL_TMP_OBJ
+set _ObjDir=TMP
 ::configuration end.
+
+set _OutFil=%_OutDir%\%_OutNam%
 
 if not exist %_OutDir% (md %_OutDir%)
 if not exist %_ObjDir% (md %_ObjDir%)
 
-pushd %_OutDir%
-    if exist %_OutDll% (del %_OutDll%)
-    if exist %_OutLib% (del %_OutLib%)
-    if exist %_OutExp% (del %_OutExp%)
-popd
+if exist %_OutFil%.dll (
+    del  %_OutFil%.dll
+)
 
-cl /Fe"%_OutDir%\\%_OutDll%" /Fo"%_ObjDir%\\" /I%_IncDir% %_CxxFlg% %_CxxSrc% %_SysLib%
+cl /Fe%_OutFil% /Fo%_ObjDir%\ /I%_IncDir% %_CxxFlg% %_CxxSrc% %_SysLib%
 
 rmdir /s /q %_ObjDir%
+
+del %_OutFil%.exp
+del %_OutFil%.lib
+
+endlocal
