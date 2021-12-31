29216f684a4fed603a8fd5571cebbe4a5484869c
wujia <wzsayiie@qq.com>
2021-12-31 19:38:14 +0800

    use space instead of tab.

--- appsrc/cboot.h
+++ appsrc/cboot.h
@@ -3,5 +3,5 @@
 #include "minikit.h"
 
 #define c_class(name) \
-/**/	typedef std::shared_ptr<class name> name##Ref; \
-/**/	class name
+/**/    typedef std::shared_ptr<class name> name##Ref; \
+/**/    class name
--- appsrc/clambda.cpp
+++ appsrc/clambda.cpp
@@ -11,31 +11,31 @@ bool CComparableLambda::operator!=(nullptr_t) const { return cmp(NoneId) != 0; }
 CComparableLambda::operator bool  ()          const { return cmp(NoneId) != 0; }
 
 void CComparableLambda::setId(void *a, void *b, void *c) {
-	_id[0] = a;
-	_id[1] = b;
-	_id[2] = c;
+    _id[0] = a;
+    _id[1] = b;
+    _id[2] = c;
 }
 
 void CComparableLambda::setRandomId() {
-	static union {
-		void   *ptr;
-		int32_t num;
-	}
-	acc = { nullptr };
+    static union {
+        void   *ptr;
+        int32_t num;
+    }
+    acc = { nullptr };
 
-	acc.num++;
+    acc.num++;
 
-	memset(_id, 0, sizeof(Id));
-	_id[0] = acc.ptr;
+    memset(_id, 0, sizeof(Id));
+    _id[0] = acc.ptr;
 }
 
 int CComparableLambda::cmp(const Id &that) const {
-	size_t count = sizeof(Id) / sizeof(void *);
+    size_t count = sizeof(Id) / sizeof(void *);
 
-	for (size_t n = 0; n < count; ++n) {
-		if (_id[n] != that[n]) {
-			return _id[n] < that[n] ? -1 : 1;
-		}
-	}
-	return 0;
+    for (size_t n = 0; n < count; ++n) {
+        if (_id[n] != that[n]) {
+            return _id[n] < that[n] ? -1 : 1;
+        }
+    }
+    return 0;
 }
