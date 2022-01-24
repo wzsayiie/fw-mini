17a11d6f90c99e3a9cd986d534fac89aad823488
wujia <wzsayiie@qq.com>
2022-01-24 18:55:46 +0800

    added api arguments check.

--- appsrc/mhostapi.cpp
+++ appsrc/mhostapi.cpp
@@ -1,5 +1,32 @@
 #include "mhostapi.h"
 
+static bool Enable_PrintMessage(MString *text) {
+    return true;
+}
+
+static bool Enable_CopyBundleAsset(MString *path) {
+    return path != nullptr;
+}
+
+static bool Enable_CreateImage(MData *data) {
+    return MDataSize(data) > 0;
+}
+
+static bool Enable_CreateBitmapImage(MData *data, int w, int h) {
+    if (w > 0 && h > 0) {
+        return w * h == MDataSize(data) * 4;
+    }
+    return false;
+}
+
+static bool Enable_CopyImageBitmap (MImage *img) { return img != nullptr; }
+static bool Enable_ImagePixelWidth (MImage *img) { return img != nullptr; }
+static bool Enable_ImagePixelHeight(MImage *img) { return img != nullptr; }
+
+static bool Enable_CopyDocumentPath () { return true; }
+static bool Enable_CopyCachePath    () { return true; }
+static bool Enable_CopyTemporaryPath() { return true; }
+
 #define IMPL(RET, NAME, ARGS, ...)                  \
 /**/    static _MApi_##NAME s##NAME = nullptr;      \
 /**/                                                \
@@ -11,9 +38,11 @@
 /**/        if (!s##NAME) {                         \
 /**/            D("ERROR: no api '%s'", "M"#NAME);  \
 /**/            return (RET)0;                      \
-/**/        } else {                                \
-/**/            return s##NAME(__VA_ARGS__);        \
 /**/        }                                       \
+/**/        if (!Enable_##NAME(__VA_ARGS__)) {      \
+/**/            return (RET)0;                      \
+/**/        }                                       \
+/**/        return s##NAME(__VA_ARGS__);            \
 /**/    }
 
 IMPL(MImage  *, CreateBitmapImage, (MData   *data , int w, int h), data, w, h);
