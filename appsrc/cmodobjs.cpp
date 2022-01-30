3693ec9347ce166e87a5b2bf927f62de47af2ef8
wzsayiie <wzsayiie@qq.com>
2022-01-30 23:25:31 +0800

    added implementation for number and string.

--- appsrc/cmodobjs.cpp
+++ appsrc/cmodobjs.cpp
@@ -11,5 +11,30 @@ CMOD_META(int64_t, IModNumber, int64Value , ());
 CMOD_META(float  , IModNumber, floatValue , ());
 CMOD_META(double , IModNumber, doubleValue, ());
 
-CMOD_META(const cmod_char *, IModString, chars , ());
-CMOD_META(int              , IModString, length, ());
+void CModNumber::setBool  (bool    value) { mInt64 = value; mDouble = value; }
+void CModNumber::setInt   (int     value) { mInt64 = value; mDouble = value; }
+void CModNumber::setInt64 (int64_t value) { mInt64 = value; mDouble = value; }
+void CModNumber::setFloat (float   value) { mInt64 = value; mDouble = value; }
+void CModNumber::setDouble(double  value) { mInt64 = value; mDouble = value; }
+
+bool    CModNumber::boolValue  () { return (bool )mInt64 ; }
+int     CModNumber::intValue   () { return (int  )mInt64 ; }
+int64_t CModNumber::int64Value () { return /*...*/mInt64 ; }
+float   CModNumber::floatValue () { return (float)mDouble; }
+double  CModNumber::doubleValue() { return /*...*/mDouble; }
+
+CMOD_META(void             , IModString, setChars, (const cmod_char *chars), chars);
+CMOD_META(const cmod_char *, IModString, chars   , ());
+CMOD_META(int              , IModString, length  , ());
+
+void CModString::setChars(const cmod_char *chars) {
+    mString = chars ? chars : CMOD_L "";
+}
+
+const cmod_char *CModString::chars() {
+    return mString.c_str();
+}
+
+int CModString::length() {
+    return (int)mString.size();
+}
--- appsrc/cmodobjs.h
+++ appsrc/cmodobjs.h
@@ -17,8 +17,40 @@ cmod_intf(IModNumber, IModObj) {
     virtual double  doubleValue();
 };
 
+class CModNumber : public IModNumber {
+    
+public:
+    void setBool  (bool    value) override;
+    void setInt   (int     value) override;
+    void setInt64 (int64_t value) override;
+    void setFloat (float   value) override;
+    void setDouble(double  value) override;
+
+    bool    boolValue  () override;
+    int     intValue   () override;
+    int64_t int64Value () override;
+    float   floatValue () override;
+    double  doubleValue() override;
+    
+private:
+    int64_t mInt64  = 0;
+    double  mDouble = 0;
+};
+
 cmod_intf(IModString, IModObj) {
 
+    virtual void setChars(const cmod_char *chars);
     virtual const cmod_char *chars();
     virtual int length();
+};
+
+class CModString : public IModString {
+    
+public:
+    void setChars(const cmod_char *chars) override;
+    const cmod_char *chars() override;
+    int length() override;
+    
+private:
+    cmod_string mString;
 };
