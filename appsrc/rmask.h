e9e24388aef614b4f7f5914e1e927bbb99924f89
wujia <wzsayiie@qq.com>
2022-02-21 14:06:43 +0800

    added mark macros.

--- appsrc/rmask.h
+++ appsrc/rmask.h
@@ -2,6 +2,24 @@
 
 #include "rset.h"
 
+//generation mark:
+//
+
+#define reflectable_class(Name)                                 \
+/**/    template<> struct reflect::type_name_of<class Name> {   \
+/**/        static constexpr const char *const name = #Name;    \
+/**/    };
+
+#define reflectable_class_function(Class, Function)                         \
+/**/    static reflect::function_commit _unused_var_##Class##_##Function(   \
+/**/        #Class, #Function, &Class::Function                             \
+/**/    );
+
+#define reflectable_function(Function)                          \
+/**/    static reflect::function_commit _unused_var_##Function( \
+/**/        #Function, Function                                 \
+/**/    );
+
 namespace reflect {
 
 //categoroies:
@@ -43,16 +61,31 @@ template<> struct type_name_of<float      > { static constexpr const char *const
 template<> struct type_name_of<double     > { static constexpr const char *const name = "double"; };
 template<> struct type_name_of<std::string> { static constexpr const char *const name = "string"; };
 
-#define reflect_reflectable_class(Name)                         \
-/**/    template<> struct reflect::type_name_of<class Name> {   \
-/**/        static constexpr const char *const name = #Name;    \
-/**/    }
+reflectable_class(set<bool>       )
+reflectable_class(set<int>        )
+reflectable_class(set<int64_t>    )
+reflectable_class(set<float>      )
+reflectable_class(set<double>     )
+reflectable_class(set<std::string>)
 
-reflect_reflectable_class(set<bool>       );
-reflect_reflectable_class(set<int>        );
-reflect_reflectable_class(set<int64_t>    );
-reflect_reflectable_class(set<float>      );
-reflect_reflectable_class(set<double>     );
-reflect_reflectable_class(set<std::string>);
+//function extraction:
+//
+
+struct function_commit {
+    template<class Ret, class Class, class... Args> function_commit(
+        const char *const class_name, const char *fcn_name, Ret (Class::*fcn)(Args...))
+    {
+    }
+
+    template<class Ret, class... Args> function_commit(
+        const char *const class_name, const char *fcn_name, Ret (*fcn)(Args...))
+    {
+    }
+
+    template<class Ret, class... Args> function_commit(
+        const char *fcn_name, Ret (*fcn)(Args...))
+    {
+    }
+};
 
 } // end reflect.
