f63da35a7c20bd4f65fa2ecb96ce596feaeba49e
wzsayiie <wzsayiie@qq.com>
2022-02-26 00:06:07 +0800

    added print function.

--- appsrc/dash.h
+++ appsrc/dash.h
@@ -2,4 +2,5 @@
 
 #include "dcustom.h"
 #include "denviron.h"
+#include "dprint.h"
 #include "dstd.h"
---
+++ appsrc/dprint.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <sstream>
+#include "denviron.h"
+
+#if DASH_OS_ANDROID
+    #include <android/log.h>
+    inline void dash_puts(const char *message) {
+        __android_log_write(ANDROID_LOG_INFO, "zzz", message);
+    }
+#else
+    #include <cstdio>
+    inline void dash_puts(const char *message) {
+        puts(message);
+    }
+#endif
+
+namespace dash {
+
+template<class Last> void print_with_stream(std::stringstream &stream, Last last) {
+    stream << last;
+    dash_puts(stream.str().c_str());
+}
+
+template<class First, class... Others> void print_with_stream(std::stringstream &stream, First first, Others... others) {
+    stream << first << " ";
+    print_with_stream(stream, others...);
+}
+
+template<class... Args> void print(Args... args) {
+    std::stringstream stream;
+    print_with_stream(stream, args...);
+}
+
+} // end dash.
