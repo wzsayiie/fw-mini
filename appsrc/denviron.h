7a4ae3b056573713ab3eddb9fd4d7db4e45bd28a
wzsayiie <wzsayiie@qq.com>
2022-02-26 16:29:11 +0800

    adjusted the os identifier.

--- appsrc/denviron.h
+++ appsrc/denviron.h
@@ -30,15 +30,15 @@
 
 //which of the following os platform.
 //
-// +-----------------------------------------+ +---------+
-// |                   posix                 | | windows |
-// | +-------------------+ +---------------+ | |         |
-// | |     linux os      | |    darwin     | | | +-----+ |
-// | | +-------+ +-----+ | | +---+ +-----+ | | | |     | |
-// | | |android| |linux| | | |ios| |macos| | | | |win32| |
-// | | +-------+ +-----+ | | +---+ +-----+ | | | |     | |
-// | +-------------------+ +---------------+ | | +-----+ |
-// +-----------------------------------------+ +---------+
+// +---------------------------------------+ +---------+
+// |                   posix               | | windows |
+// | +-------------------+ +-------------+ | |         |
+// | |     linux os      | |     mac     | | | +-----+ |
+// | | +-------+ +-----+ | | +---+ +---+ | | | |     | |
+// | | |android| |linux| | | |ios| |osx| | | | |win32| |
+// | | +-------+ +-----+ | | +---+ +---+ | | | |     | |
+// | +-------------------+ +-------------+ | | +-----+ |
+// +---------------------------------------+ +---------+
 //
 #if __APPLE__
     #include <TargetConditionals.h>
@@ -47,31 +47,31 @@
     #define DASH_OS_ANDROID 1
     #define DASH_OS_IOS     0
     #define DASH_OS_WIN32   0
-    #define DASH_OS_MACOS   0
+    #define DASH_OS_OSX     0
     #define DASH_OS_LINUX   0
 #elif TARGET_OS_IOS
     #define DASH_OS_ANDROID 0
     #define DASH_OS_IOS     1
     #define DASH_OS_WIN32   0
-    #define DASH_OS_MACOS   0
+    #define DASH_OS_OSX     0
     #define DASH_OS_LINUX   0
 #elif _WIN32
     #define DASH_OS_ANDROID 0
     #define DASH_OS_IOS     0
     #define DASH_OS_WIN32   1
-    #define DASH_OS_MACOS   0
+    #define DASH_OS_OSX     0
     #define DASH_OS_LINUX   0
 #elif TARGET_OS_OSX
     #define DASH_OS_ANDROID 0
     #define DASH_OS_IOS     0
     #define DASH_OS_WIN32   0
-    #define DASH_OS_MACOS   1
+    #define DASH_OS_OSX     1
     #define DASH_OS_LINUX   0
 #elif __linux__
     #define DASH_OS_ANDROID 0
     #define DASH_OS_IOS     0
     #define DASH_OS_WIN32   0
-    #define DASH_OS_MACOS   0
+    #define DASH_OS_OSX     0
     #define DASH_OS_LINUX   1
 #else
     #error "unknown os platform."
