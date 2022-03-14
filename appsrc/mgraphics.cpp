33411d9a32f592fa3ecce81992448e91089a9b7a
wujia <wzsayiie@qq.com>
2022-03-14 18:09:05 +0800

    added alignment and color type.

--- appsrc/mgraphics.cpp
+++ appsrc/mgraphics.cpp
@@ -22,3 +22,77 @@ float _MPointFromPixelCaster::operator<<(float px) {
 float _MPixelFromPointCaster::operator<<(float pt) {
     return pt * sPixelScale;
 }
+
+//alignment:
+//
+
+define_reflectable_enum_const(MHAlign, Left  )
+define_reflectable_enum_const(MHAlign, Center)
+define_reflectable_enum_const(MHAlign, Right )
+
+define_reflectable_enum_const(MVAlign, Top   )
+define_reflectable_enum_const(MVAlign, Middle)
+define_reflectable_enum_const(MVAlign, Bottom)
+
+//color:
+//
+
+define_reflectable_class_const(MColor, BlackRGBA    )
+define_reflectable_class_const(MColor, DarkGrayRGBA )
+define_reflectable_class_const(MColor, GrayRGBA     )
+define_reflectable_class_const(MColor, LightGrayRGBA)
+define_reflectable_class_const(MColor, WhiteRGBA    )
+define_reflectable_class_const(MColor, RedRGBA      )
+define_reflectable_class_const(MColor, GreenRGBA    )
+define_reflectable_class_const(MColor, BlueRGBA     )
+define_reflectable_class_const(MColor, CyanRGBA     )
+define_reflectable_class_const(MColor, YellowRGBA   )
+define_reflectable_class_const(MColor, MagentaRGBA  )
+define_reflectable_class_const(MColor, OrangeRGBA   )
+define_reflectable_class_const(MColor, PurpleRGBA   )
+define_reflectable_class_const(MColor, BrownRGBA    )
+define_reflectable_class_const(MColor, ClearRGBA    )
+
+MColor::MColor() {
+}
+
+MColor::MColor(float redCom, float greenCom, float blueCom, float alphaCom) {
+    _rgba.red   = (uint8_t)(redCom   * 255.f);
+    _rgba.green = (uint8_t)(greenCom * 255.f);
+    _rgba.blue  = (uint8_t)(blueCom  * 255.f);
+    _rgba.alpha = (uint8_t)(alphaCom * 255.f);
+}
+
+MColor::MColor(int rgba) {
+    _rgba.rgba = rgba;
+}
+
+define_reflectable_class_function(MColor, redCom)
+float MColor::redCom() {
+    return _rgba.red / 255.f;
+}
+
+define_reflectable_class_function(MColor, greenCom)
+float MColor::greenCom() {
+    return _rgba.green / 255.f;
+}
+
+define_reflectable_class_function(MColor, blueCom)
+float MColor::blueCom() {
+    return _rgba.blue / 255.f;
+}
+
+define_reflectable_class_function(MColor, alphaCom)
+float MColor::alphaCom() {
+    return _rgba.alpha / 255.f;
+}
+
+define_reflectable_class_function(MColor, rgba)
+int MColor::rgba() {
+    return _rgba.rgba;
+}
+
+define_reflectable_class_function(MColor, copy)
+MColor::ptr MColor::copy() {
+    return MColor::create(_rgba.rgba);
+}
--- appsrc/mgraphics.h
+++ appsrc/mgraphics.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include "reflect.h"
+#include "mobject.h"
 
 //pixel scale:
 //
 
-void  MSetPixelScale(float scale);
+M_HOST_CALL void MSetPixelScale(float scale);
 float MGetPixelScale();
 
 #define pt_from_px _MPointFromPixelCaster()<<
@@ -13,3 +13,95 @@ float MGetPixelScale();
 
 struct _MPointFromPixelCaster { float operator<<(float px); };
 struct _MPixelFromPointCaster { float operator<<(float pt); };
+
+//alignment:
+//
+
+declare_reflectable_enum(MHAlign)
+enum class MHAlign {
+    Left   = 1,
+    Center = 2,
+    Right  = 3,
+};
+
+declare_reflectable_enum(MVAlign)
+enum class MVAlign {
+    Top    = 1,
+    Middle = 2,
+    Bottom = 3,
+};
+
+//color:
+//
+
+union MColorRGBA {
+    int rgba = 0;
+
+    struct {
+        uint8_t alpha;
+        uint8_t blue ;
+        uint8_t green;
+        uint8_t red  ;
+    };
+};
+
+//android and win32 apis use the order of a-r-g-b.
+union MColorARGB {
+    int argb = 0;
+
+    struct {
+        uint8_t blue ;
+        uint8_t green;
+        uint8_t red  ;
+        uint8_t alpha;
+    };
+};
+
+//mac apis use the order of a-b-g-r.
+union MColorABGR {
+    int abgr = 0;
+
+    struct {
+        uint8_t alpha;
+        uint8_t blue ;
+        uint8_t green;
+        uint8_t red  ;
+    };
+};
+
+declare_reflectable_class(MColor)
+class MColor : public MExtends<MColor, MObject> {
+public:
+    static const int BlackRGBA     = 0x000000ff;
+    static const int DarkGrayRGBA  = 0x404040ff;
+    static const int GrayRGBA      = 0x808080ff;
+    static const int LightGrayRGBA = 0xC0C0C0ff;
+    static const int WhiteRGBA     = 0xFFFFFFff;
+    static const int RedRGBA       = 0xC3272Bff;
+    static const int GreenRGBA     = 0x21A675ff;
+    static const int BlueRGBA      = 0x177CB0ff;
+    static const int CyanRGBA      = 0x4C8DAEff;
+    static const int YellowRGBA    = 0xF2BE45ff;
+    static const int MagentaRGBA   = 0xA01DAEff;
+    static const int OrangeRGBA    = 0xFFA400ff;
+    static const int PurpleRGBA    = 0x8D4BBBff;
+    static const int BrownRGBA     = 0xE29C45ff;
+    static const int ClearRGBA     = 0x00000000;
+
+public:
+    MColor();
+    MColor(float redCom, float greenCom, float blueCom, float alphaCom);
+    MColor(int rgba);
+
+    float redCom  ();
+    float greenCom();
+    float blueCom ();
+    float alphaCom();
+
+    int rgba();
+
+    MColor::ptr copy();
+
+private:
+    MColorRGBA _rgba;
+};
