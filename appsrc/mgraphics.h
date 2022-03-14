0d7c4bfc54d9029a230e912b2f9e7e66be6e82ec
wujia <wzsayiie@qq.com>
2022-03-14 18:15:24 +0800

    fixed the bug about color component order.

--- appsrc/mgraphics.h
+++ appsrc/mgraphics.h
@@ -62,10 +62,10 @@ union MColorABGR {
     int abgr = 0;
 
     struct {
-        uint8_t alpha;
-        uint8_t blue ;
-        uint8_t green;
         uint8_t red  ;
+        uint8_t green;
+        uint8_t blue ;
+        uint8_t alpha;
     };
 };
 
