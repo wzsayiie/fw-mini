e413e8ea86dca85c229f8b9a86b67fc592acc146
wujia <wzsayiie@qq.com>
2022-03-19 16:22:36 +0800

    added byte type support.

--- appsrc/rutility.cpp
+++ appsrc/rutility.cpp
@@ -57,6 +57,7 @@ any::any(nullptr_t) {
 }
 
 any::any(bool    value) { _type = data_type::is_bool  ; _word.as_bool   = value; }
+any::any(uint8_t value) { _type = data_type::is_byte  ; _word.as_byte   = value; }
 any::any(int     value) { _type = data_type::is_int   ; _word.as_int    = value; }
 any::any(int64_t value) { _type = data_type::is_int64 ; _word.as_int64  = value; }
 any::any(float   value) { _type = data_type::is_float ; _word.as_float  = value; }
@@ -76,6 +77,10 @@ bool any::as_bool() const {
     return !!as_int64() || !_string.empty() || (bool)_object;
 }
 
+uint8_t any::as_byte() const {
+    return (uint8_t)as_int64();
+}
+
 int any::as_int() const {
     return (int)as_int64();
 }
@@ -83,6 +88,7 @@ int any::as_int() const {
 int64_t any::as_int64() const {
     switch (_type) {
         case data_type::is_bool  : return (int64_t)_word.as_bool  ;
+        case data_type::is_byte  : return (int64_t)_word.as_byte  ;
         case data_type::is_int   : return (int64_t)_word.as_int   ;
         case data_type::is_int64 : return /* ... */_word.as_int64 ;
         case data_type::is_float : return (int64_t)_word.as_float ;
@@ -99,6 +105,7 @@ float any::as_float() const {
 double any::as_double() const {
     switch (_type) {
         case data_type::is_bool  : return (double)_word.as_bool  ;
+        case data_type::is_byte  : return (double)_word.as_byte  ;
         case data_type::is_int   : return (double)_word.as_int   ;
         case data_type::is_int64 : return (double)_word.as_int64 ;
         case data_type::is_float : return (double)_word.as_float ;
@@ -117,6 +124,7 @@ const dash::object::ptr &any::as_object() const {
 }
 
 any::operator bool   () const { return as_bool  (); }
+any::operator uint8_t() const { return as_byte  (); }
 any::operator int    () const { return as_int   (); }
 any::operator int64_t() const { return as_int64 (); }
 any::operator float  () const { return as_float (); }
--- appsrc/rutility.h
+++ appsrc/rutility.h
@@ -42,6 +42,7 @@ public:
     any(nullptr_t);
     
     any(bool    value);
+    any(uint8_t value);
     any(int     value);
     any(int64_t value);
     any(float   value);
@@ -49,8 +50,14 @@ public:
     
     any(const std::string       &value);
     any(const dash::object::ptr &value);
+
+    template<class Type> any(const Type &value)
+        : any(std::static_pointer_cast<dash::object>(value))
+    {
+    }
     
     bool    as_bool  () const;
+    uint8_t as_byte  () const;
     int     as_int   () const;
     int64_t as_int64 () const;
     float   as_float () const;
@@ -60,6 +67,7 @@ public:
     const dash::object::ptr &as_object() const;
 
     operator bool   () const;
+    operator uint8_t() const;
     operator int    () const;
     operator int64_t() const;
     operator float  () const;
@@ -75,12 +83,15 @@ public:
 private:
     enum class data_type {
         is_void  ,
-        is_bool  , is_int   , is_int64, is_float, is_double,
+        is_bool  ,
+        is_byte  , is_int   , is_int64 ,
+        is_float , is_double,
         is_string, is_object,
     };
     union data_word {
         int64_t as_void  ;
         bool    as_bool  ;
+        uint8_t as_byte  ;
         int     as_int   ;
         int64_t as_int64 ;
         float   as_float ;
