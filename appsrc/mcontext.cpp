ff625f750bfbd22dfa3ddf1d2bbb5d070408a76d
wujia <wzsayiie@qq.com>
2022-03-24 21:11:04 +0800

    added context function declarations.

--- appsrc/mcontext.cpp
+++ appsrc/mcontext.cpp
@@ -1,1 +1,214 @@
 #include "mcontext.h"
+
+//context data:
+//
+
+static dash::lazy_var<MVector<MGraph::ptr>> sGraphs;
+
+static dash::lazy_var<MVector<MPoint>> sOffsetStack;
+static dash::lazy_var<MVector<MRect >> sClipStack;
+static float sOffsetX = 0;
+static float sOffsetY = 0;
+
+static dash::lazy_var<MImage::ptr> sImage;
+static dash::lazy_var<std::string> sText;
+static float   sFontSize  = 0;
+static float   sLineWidth = 0;
+static MHAlign sHAlign    = MHAlign::Center;
+static MVAlign sVAlign    = MVAlign::Middle;
+static int     sRGBA      = 0;
+
+//draw function:
+//
+
+define_reflectable_function(MContextSelectImage, "args:image;")
+void MContextSelectImage(const MImage::ptr &image) {
+}
+
+define_reflectable_function(MContextSelectText, "args:text;")
+void MContextSelectText(const std::string &text) {
+}
+
+define_reflectable_function(MContextSelectFontSize, "args:size;")
+void MContextSelectFontSize(float size) {
+}
+
+define_reflectable_function(MContextSelectLineWidth, "args:width;")
+void MContextSelectLineWidth(float width) {
+}
+
+define_reflectable_function(MContextSelectHAlign, "args:align;")
+void MContextSelectHAlign(MHAlign align) {
+}
+
+define_reflectable_function(MContextSelectVAlign, "args:align;")
+void MContextSelectVAlign(MVAlign align) {
+}
+
+define_reflectable_function(MContextSelectRGBA, "args:rgba;")
+void MContextSelectRGBA(int rgba) {
+}
+
+define_reflectable_function(MContextPushClip, "args:x,y,w,h;")
+void MContextPushClip(float x, float y, float w, float h) {
+}
+
+define_reflectable_function(MContextPopClip)
+void MContextPopClip() {
+}
+
+define_reflectable_function(MContextPushOffset, "args:x,y;")
+void MContextPushOffset(float x, float y) {
+}
+
+define_reflectable_function(MContextPopOffset)
+void MContextPopOffset() {
+}
+
+define_reflectable_function(MContextDrawTriangle, "args:x0,y0,x1,y1,x2,y2;")
+void MContextDrawTriangle(float x0, float y0, float x1, float y1, float x2, float y2) {
+}
+
+define_reflectable_function(MContextDrawRect, "args:x,y,w,h;")
+void MContextDrawRect(float x, float y, float w, float h) {
+}
+
+define_reflectable_function(MContextDrawEllipse, "args:x,y,w,h;")
+void MContextDrawEllipse(float x, float y, float w, float h) {
+}
+
+define_reflectable_function(MContextDrawFlatLine, "args:x0,y0,x1,y1;")
+void MContextDrawFlatLine(float x0, float y0, float x1, float y1) {
+}
+
+define_reflectable_function(MContextDrawSquareLine, "args:x0,y0,x1,y1;")
+void MContextDrawSquareLine(float x0, float y0, float x1, float y1) {
+}
+
+define_reflectable_function(MContextDrawRoundLine, "args:x0,y0,x1,y1;")
+void MContextDrawRoundLine(float x0, float y0, float x1, float y1) {
+}
+
+define_reflectable_function(MContextDrawImage, "args:x,y,w,h;")
+void MContextDrawImage(float x, float y, float w, float h) {
+}
+
+define_reflectable_function(MContextDrawString, "args:x,y,w,h;")
+void MContextDrawString(float x, float y, float w, float h) {
+}
+
+//graphs:
+//
+
+define_reflectable_enum_const(MGraphType, Clip    )
+define_reflectable_enum_const(MGraphType, Triangle)
+define_reflectable_enum_const(MGraphType, Image   )
+define_reflectable_enum_const(MGraphType, Text    )
+
+//clip graph:
+//
+
+define_reflectable_class_function(MClipGraph, type)
+MGraphType MClipGraph::type() {
+    implement_injectable_function((MGraphType)(int))
+    return MGraphType::Clip;
+}
+
+define_reflectable_class_function(MClipGraph, pixelX)
+define_reflectable_class_function(MClipGraph, pixelY)
+define_reflectable_class_function(MClipGraph, pixelW)
+define_reflectable_class_function(MClipGraph, pixelH)
+
+float MClipGraph::pixelX() { return m_px_from_pt mX; }
+float MClipGraph::pixelY() { return m_px_from_pt mY; }
+float MClipGraph::pixelW() { return m_px_from_pt mW; }
+float MClipGraph::pixelH() { return m_px_from_pt mH; }
+
+//triangle graph:
+//
+
+define_reflectable_class_function(MTriangleGraph, type)
+MGraphType MTriangleGraph::type() {
+    implement_injectable_function((MGraphType)(int))
+    return MGraphType::Triangle;
+}
+
+define_reflectable_class_function(MTriangleGraph, rgba   )
+define_reflectable_class_function(MTriangleGraph, pixelX0)
+define_reflectable_class_function(MTriangleGraph, pixelY0)
+define_reflectable_class_function(MTriangleGraph, pixelX1)
+define_reflectable_class_function(MTriangleGraph, pixelY1)
+define_reflectable_class_function(MTriangleGraph, pixelX2)
+define_reflectable_class_function(MTriangleGraph, pixelY2)
+
+int   MTriangleGraph::rgba   () { return mRGBA           ; }
+float MTriangleGraph::pixelX0() { return m_px_from_pt mX0; }
+float MTriangleGraph::pixelY0() { return m_px_from_pt mY0; }
+float MTriangleGraph::pixelX1() { return m_px_from_pt mX1; }
+float MTriangleGraph::pixelY1() { return m_px_from_pt mY1; }
+float MTriangleGraph::pixelX2() { return m_px_from_pt mX2; }
+float MTriangleGraph::pixelY2() { return m_px_from_pt mY2; }
+
+//image graph:
+//
+
+define_reflectable_class_function(MImageGraph, type)
+MGraphType MImageGraph::type() {
+    implement_injectable_function((MGraphType)(int))
+    return MGraphType::Image;
+}
+
+define_reflectable_class_function(MImageGraph, image )
+define_reflectable_class_function(MImageGraph, pixelX)
+define_reflectable_class_function(MImageGraph, pixelY)
+define_reflectable_class_function(MImageGraph, pixelW)
+define_reflectable_class_function(MImageGraph, pixelH)
+
+MImage::ptr MImageGraph::image() { return mImage; }
+
+float MImageGraph::pixelX() { return m_px_from_pt mX; }
+float MImageGraph::pixelY() { return m_px_from_pt mY; }
+float MImageGraph::pixelW() { return m_px_from_pt mW; }
+float MImageGraph::pixelH() { return m_px_from_pt mH; }
+
+//text graph:
+//
+
+define_reflectable_class_function(MTextGraph, type)
+MGraphType MTextGraph::type() {
+    implement_injectable_function((MGraphType)(int))
+    return MGraphType::Image;
+}
+
+define_reflectable_class_function(MTextGraph, text    )
+define_reflectable_class_function(MTextGraph, rgba    )
+define_reflectable_class_function(MTextGraph, fontSize)
+define_reflectable_class_function(MTextGraph, hAlign  )
+define_reflectable_class_function(MTextGraph, vAlign  )
+define_reflectable_class_function(MTextGraph, pixelX  )
+define_reflectable_class_function(MTextGraph, pixelY  )
+define_reflectable_class_function(MTextGraph, pixelW  )
+define_reflectable_class_function(MTextGraph, pixelH  )
+
+std::string MTextGraph::text() { return mText; }
+
+int     MTextGraph::rgba    () { return mRGBA    ; }
+float   MTextGraph::fontSize() { return mFontSize; }
+MHAlign MTextGraph::hAlign  () { return mHAlign  ; }
+MVAlign MTextGraph::vAlign  () { return mVAlign  ; }
+
+float MTextGraph::pixelX() { return m_px_from_pt mX; }
+float MTextGraph::pixelY() { return m_px_from_pt mY; }
+float MTextGraph::pixelW() { return m_px_from_pt mW; }
+float MTextGraph::pixelH() { return m_px_from_pt mH; }
+
+//host functions:
+//
+
+MVector<MGraph::ptr> *MContextGetGraphs() {
+    return &sGraphs;
+}
+
+void MContextReset() {
+    sGraphs->vector.clear();
+}
--- appsrc/mcontext.h
+++ appsrc/mcontext.h
@@ -1,1 +1,156 @@
 #pragma once
+
+#include "mgraphics.h"
+#include "mimage.h"
+
+//draw function:
+//
+
+void MContextSelectImage    (const MImage::ptr &image);
+void MContextSelectText     (const std::string &text );
+void MContextSelectFontSize (float   size );
+void MContextSelectLineWidth(float   width);
+void MContextSelectHAlign   (MHAlign align);
+void MContextSelectVAlign   (MVAlign align);
+void MContextSelectRGBA     (int     rgba );
+
+void MContextPushClip  (float x, float y, float w, float h);
+void MContextPopClip   ();
+void MContextPushOffset(float x, float y);
+void MContextPopOffset ();
+
+void MContextDrawTriangle  (float x0, float y0, float x1, float y1, float x2, float y2);
+void MContextDrawRect      (float x , float y , float w , float h );
+void MContextDrawEllipse   (float x , float y , float w , float h );
+void MContextDrawFlatLine  (float x0, float y0, float x1, float y1);
+void MContextDrawSquareLine(float x0, float y0, float x1, float y1);
+void MContextDrawRoundLine (float x0, float y0, float x1, float y1);
+void MContextDrawImage     (float x , float y , float w , float h );
+void MContextDrawString    (float x , float y , float w , float h );
+
+//graphs:
+//
+
+declare_reflectable_enum(MGraphType)
+enum class MGraphType {
+    Clip     = 1,
+    Triangle = 2,
+    Image    = 3,
+    Text     = 4,
+};
+
+declare_reflectable_class(MGraph)
+class MGraph : public MExtends<MGraph, MObject> {
+public:
+    virtual MGraphType type() = 0;
+};
+
+//clip graph:
+//
+
+declare_reflectable_class(MClipGraph)
+class MClipGraph : public MExtends<MClipGraph, MGraph> {
+public:
+    MGraphType type() override;
+
+    float pixelX();
+    float pixelY();
+    float pixelW();
+    float pixelH();
+
+public:
+    float mX = 0;
+    float mY = 0;
+    float mW = 0;
+    float mH = 0;
+};
+
+//triangle graph:
+//
+
+declare_reflectable_class(MTriangleGraph)
+class MTriangleGraph : public MExtends<MTriangleGraph, MGraph> {
+public:
+    MGraphType type() override;
+
+    int   rgba   ();
+    float pixelX0();
+    float pixelY0();
+    float pixelX1();
+    float pixelY1();
+    float pixelX2();
+    float pixelY2();
+
+public:
+    int   mRGBA = 0;
+    float mX0   = 0;
+    float mY0   = 0;
+    float mX1   = 0;
+    float mY1   = 0;
+    float mX2   = 0;
+    float mY2   = 0;
+};
+
+//image graph:
+//
+
+declare_reflectable_class(MImageGraph)
+class MImageGraph : public MExtends<MImageGraph, MGraph> {
+public:
+    MGraphType type() override;
+
+    MImage::ptr image();
+
+    float pixelX();
+    float pixelY();
+    float pixelW();
+    float pixelH();
+
+public:
+    MImage::ptr mImage;
+
+    float mX = 0;
+    float mY = 0;
+    float mW = 0;
+    float mH = 0;
+};
+
+//text graph:
+//
+
+declare_reflectable_class(MTextGraph)
+class MTextGraph : public MExtends<MTextGraph, MGraph> {
+public:
+    MGraphType type() override;
+
+    std::string text();
+
+    int     rgba    ();
+    float   fontSize();
+    MHAlign hAlign  ();
+    MVAlign vAlign  ();
+    float   pixelX  ();
+    float   pixelY  ();
+    float   pixelW  ();
+    float   pixelH  ();
+
+public:
+    std::string mText;
+
+    int     mRGBA     = 0;
+    float   mFontSize = 0;
+    MHAlign mHAlign   = MHAlign::Center;
+    MVAlign mVAlign   = MVAlign::Middle;
+    float   mX        = 0;
+    float   mY        = 0;
+    float   mW        = 0;
+    float   mH        = 0;
+};
+
+//host functions:
+//
+
+declare_reflectable_special(MVector<MGraph::ptr>)
+
+M_HOST_CALL_FUNCTION MVector<MGraph::ptr> *MContextGetGraphs();
+M_HOST_CALL_FUNCTION void MContextReset();
