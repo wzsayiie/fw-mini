954ade7a9b11430d1783558e3fb4b5351072e844
wujia <wzsayiie@qq.com>
2022-03-31 21:06:10 +0800

    optimized.

--- makelib_win32.bat
+++ makelib_win32.bat
@@ -4,32 +4,81 @@ setlocal
 
 cd %~dp0
 
-::configuration begin.
-set _IncDir=appsrc
-set _CxxSrc=appsrc\*.cpp host_win32\mini\*.cpp
-set _SysLib=chakrart.lib gdi32.lib gdiplus.lib ole32.lib shell32.lib user32.lib
-set _CxxFlg=/nologo /std:c++17 /EHsc /Ox /MD /LD
+set _flg= ^
+    /nologo ^
+    /std:c++17 ^
+    /EHsc ^
+    /MD ^
+    /LD
 
-set _OutDir=BUILD
-set _OutNam=mini
+set _inc= ^
+    /I"appsrc" ^
+    /I"host_win32\mini_win32" ^
+    /I"host\mini_pc"
 
-set _ObjDir=TMP
-::configuration end.
+set _src= ^
+    appsrc\*.cpp ^
+    host_win32\mini_win32\*.cpp ^
+    host\mini_pc\*.cpp
 
-set _OutFil=%_OutDir%\%_OutNam%
+set _lib= ^
+    chakrart.lib ^
+    gdi32.lib ^
+    gdiplus.lib ^
+    ole32.lib ^
+    shell32.lib ^
+    user32.lib
 
-if not exist %_OutDir% (md %_OutDir%)
-if not exist %_ObjDir% (md %_ObjDir%)
+:main
+    call :build "x64" "BUILD\windows_x64" "mini"
+    call :build "x86" "BUILD\windows_x86" "mini"
+goto :eof
 
-if exist %_OutFil%.dll (
-    del  %_OutFil%.dll
-)
+:build
+    set plat=%~1
+    set ddir=%~2
+    set name=%~3
 
-cl /Fe%_OutFil% /Fo%_ObjDir%\ /I%_IncDir% %_CxxFlg% %_CxxSrc% %_SysLib%
+    set file=%ddir%\%name%
+    set objs=%ddir%\objs\
 
-rmdir /s /q %_ObjDir%
+    call :newdir  %objs%
+    call :delfile %file%.dll
+    call :delfile %file%.lib
+    call :delfile %file%.exp
 
-del %_OutFil%.exp
-del %_OutFil%.lib
+    ::build machine should be x64 platform.
+    if "%plat%"=="x64" ( call vcvarsall x64     )
+    if "%plat%"=="x86" ( call vcvarsall x64_x86 )
+
+    cl  /Fe%file% ^
+        /fd%file% ^
+        /Fo%objs% ^
+        %_flg% ^
+        %_inc% ^
+        %_src% ^
+        %_lib%
+
+    call :deldir %objs%
+
+goto :eof
+
+:newdir
+    if not exist %~1 (
+        md %~1
+    )
+goto :eof
+
+:delfile
+    if exist %~1 (
+        del %~1
+    )
+goto :eof
+
+:deldir
+    if exist %~1 (
+        rd /s/q %~1
+    )
+goto :eof
 
 endlocal
