0aa20c41f427c4a6c93ab1ef4b4bc4d2c828dae3
wujia <wzsayiie@qq.com>
2022-04-14 19:59:03 +0800

    adjusted header inclusion.

--- appsrc/mencode.h
+++ appsrc/mencode.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <string>
+#include "mobject.h"
 
 int MReadU8 (const char     *begin, const char     *end, char32_t *outCode);
 int MReadU16(const char16_t *begin, const char16_t *end, char32_t *outCode);
