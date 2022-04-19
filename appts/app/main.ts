ba07344207404bbbb6352690f9431c3be98d87d4
wujia <wzsayiie@qq.com>
2022-04-19 18:09:21 +0800

    removed the typescript project.

--- appts/app/main.ts
+++
@@ -1,4 +0,0 @@
-import { Camera } from './gamekit/Camera'
-import { Device } from './gamekit/Device'
-import { Log    } from './commkit/Log'
-import { Sprite } from './gamekit/Sprite'
--- appts/wp-app.js
+++
@@ -1,6 +0,0 @@
-const config = require('./wp-uni')
-
-config.entry = './app/main.ts'
-config.module.rules[0].options.configFile = 'tsconfig.json'
-
-module.exports = config
