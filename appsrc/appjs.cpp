2ea0804ab1ae2c7bbf70a25869badbe5eec8a166
wzsayiie <wzsayiie@qq.com>
2022-06-12 21:36:21 +0800

    adjusted js app.

--- appsrc/appjs.cpp
+++ appsrc/appjs.cpp
@@ -1,13 +1,30 @@
 #include "minikit.h"
 
+static void onException(const std::string &message) {
+    MPrint(message);
+}
+
+static void registerNativeFunctions() {
+}
+
+static void registerNativeMetas() {
+}
+
+static void registerBuiltinFunctions() {
+}
+
+static void registerBuiltinScript() {
+}
+
 static void launch() m_app_launch(launch) {
+    MJsVM::instance()->setExceptionListener(MF(onException));
+    
+    registerNativeFunctions();
+    registerNativeMetas();
+    registerBuiltinFunctions();
+    registerBuiltinScript();
 
-    auto listener = MFunction<void (const std::string &)>::create([](const std::string &message) {
-        dash::print(message);
-    });
-    MJsVM::instance()->setExceptionListener(listener);
-
-    std::string name   = "bundle.js";
-    std::string script = MFileManager::instance()->u8stringFromBundle(name);
-    MJsVM::instance()->evaluate(name, script);
+    std::string name = "bundle.js";
+    std::string code = MFileManager::instance()->u8stringFromBundle(name);
+    MJsVM::instance()->evaluate(name, code);
 }
