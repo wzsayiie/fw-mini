90361dfda68f982f9d95bfd9bc3a74bb76be8e74
wujia <wzsayiie@qq.com>
2022-07-13 19:10:45 +0800

    simplified focus transferring.

--- appsrc/ccontrol.cpp
+++ appsrc/ccontrol.cpp
@@ -37,12 +37,21 @@ std::string CControl::controlId() {
     return mControlId;
 }
 
+define_reflectable_class_function(CControl, setPreviousControl, "setter;args:iden")
+define_reflectable_class_function(CControl, setNextControl    , "setter;args:iden")
+
+void CControl::setPreviousControl(const std::string &iden) { setControl(&mPreviousControl, iden); }
+void CControl::setNextControl    (const std::string &iden) { setControl(&mNextControl    , iden); }
+
+define_reflectable_class_function(CControl, previousControl, "getter")
+define_reflectable_class_function(CControl, nextControl    , "getter")
+
+std::string CControl::previousControl() { return controlIdOf(&mPreviousControl); }
+std::string CControl::nextControl    () { return controlIdOf(&mNextControl    ); }
+
 define_reflectable_class_function(CControl, transferFocusControl)
 void CControl::transferFocusControl() {
-    transfer({
-        &mRightControl, &mNextControl ,
-        &mLeftControl , &mFrontControl
-    });
+    transfer({ &mNextControl, &mPreviousControl });
 
     //abandon the focus if it has not been transferred.
     if (isFocusResponder()) {
@@ -50,32 +59,12 @@ void CControl::transferFocusControl() {
     }
 }
 
-define_reflectable_class_function(CControl, setFrontControl, "setter;args:iden")
-define_reflectable_class_function(CControl, setNextControl , "setter;args:iden")
-define_reflectable_class_function(CControl, setLeftControl , "setter;args:iden")
-define_reflectable_class_function(CControl, setRightControl, "setter;args:iden")
-
-void CControl::setFrontControl(const std::string &iden) { setControl(&mFrontControl, iden); }
-void CControl::setNextControl (const std::string &iden) { setControl(&mNextControl , iden); }
-void CControl::setLeftControl (const std::string &iden) { setControl(&mLeftControl , iden); }
-void CControl::setRightControl(const std::string &iden) { setControl(&mRightControl, iden); }
-
-define_reflectable_class_function(CControl, frontControl, "getter")
-define_reflectable_class_function(CControl, nextControl , "getter")
-define_reflectable_class_function(CControl, leftControl , "getter")
-define_reflectable_class_function(CControl, rightControl, "getter")
-
-std::string CControl::frontControl() { return controlIdOf(&mFrontControl); }
-std::string CControl::nextControl () { return controlIdOf(&mNextControl ); }
-std::string CControl::leftControl () { return controlIdOf(&mLeftControl ); }
-std::string CControl::rightControl() { return controlIdOf(&mRightControl); }
-
 void CControl::onDrawForeground(float width, float height) {
     if (!isFocusResponder()) {
         return;
     }
     
-    //mark focus responder:
+    //draw focus frame:
     MContextSelectRGBA(MColor::BlackRGBA);
     const float thick = 2.0f;
     
@@ -87,18 +76,11 @@ void CControl::onDrawForeground(float width, float height) {
 
 void CControl::onKey(MKey key) {
     switch (key) {
-        case MKey::Down : transfer({ &mNextControl , &mRightControl }); break;
-        case MKey::Right: transfer({ &mRightControl, &mNextControl  }); break;
-        case MKey::Up   : transfer({ &mFrontControl, &mLeftControl  }); break;
-        case MKey::Left : transfer({ &mLeftControl , &mFrontControl }); break;
-        
-        case MKey::Tab: {
-            transfer({
-                &mRightControl, &mNextControl ,
-                &mLeftControl , &mFrontControl
-            });
-            break;
-        }
+        case MKey::Left : transfer({ &mPreviousControl }); break;
+        case MKey::Up   : transfer({ &mPreviousControl }); break;
+        case MKey::Right: transfer({ &mNextControl     }); break;
+        case MKey::Down : transfer({ &mNextControl     }); break;
+        case MKey::Tab  : transfer({ &mNextControl     }); break;
 
         //pass the event to subclass.
         default: onControlKey(key);
@@ -135,15 +117,19 @@ std::string CControl::controlIdOf(CControl **target) {
     return (*target)->controlId();
 }
 
-void CControl::transfer(const std::initializer_list<CControl **> &ids) {
-    for (auto &iden : ids) {
+void CControl::transfer(const std::initializer_list<CControl **> &targets) {
+    for (auto &target : targets) {
+        if (!*target) {
+            continue;
+        }
+
         //the control has been released.
-        if (sSet->find(*iden) == sSet->end()) {
-            *iden = nullptr;
+        if (sSet->find(*target) == sSet->end()) {
+            *target = nullptr;
             continue;
         }
         
-        (*iden)->becomeFocusResponder();
+        (*target)->becomeFocusResponder();
         break;
     }
 }
--- appsrc/ccontrol.h
+++ appsrc/ccontrol.h
@@ -13,17 +13,13 @@ public:
     void setControlId(const std::string &iden);
     std::string controlId();
     
+    void setPreviousControl(const std::string &iden);
+    void setNextControl    (const std::string &iden);
+    
+    std::string previousControl();
+    std::string nextControl    ();
+
     void transferFocusControl();
-    
-    void setFrontControl(const std::string &iden);
-    void setNextControl (const std::string &iden);
-    void setLeftControl (const std::string &iden);
-    void setRightControl(const std::string &iden);
-    
-    std::string frontControl();
-    std::string nextControl ();
-    std::string leftControl ();
-    std::string rightControl();
     
 protected:
     void onDrawForeground(float width, float height) override;
@@ -36,13 +32,11 @@ private:
     void setControl(CControl **target, const std::string &iden);
     std::string controlIdOf(CControl **target);
 
-    void transfer(const std::initializer_list<CControl **> &ids);
+    void transfer(const std::initializer_list<CControl **> &targets);
     
 private:
     std::string mControlId;
     
-    CControl *mFrontControl = nullptr;
-    CControl *mNextControl  = nullptr;
-    CControl *mLeftControl  = nullptr;
-    CControl *mRightControl = nullptr;
+    CControl *mPreviousControl = nullptr;
+    CControl *mNextControl     = nullptr;
 };
