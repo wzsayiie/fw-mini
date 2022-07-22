151b82754eb1a4f9f0c0c103de86dfceb10de350
wzsayiie <wzsayiie@qq.com>
2022-07-22 23:52:54 +0800

    added event classes.

--- appsrc/mevent.cpp
+++ appsrc/mevent.cpp
@@ -1,1 +1,136 @@
 #include "mevent.h"
+
+//touch event:
+
+define_reflectable_class_function(MTouch, makeBegin, "args:x,y,source")
+MTouch::ptr MTouch::makeBegin(float x, float y, MTouchSource source) {
+    auto event = MTouch::create();
+    
+    event->mSource = source;
+    event->mStep   = MTouchStep::Begin;
+    event->mX      = x;
+    event->mY      = y;
+    
+    return event;
+}
+
+define_reflectable_class_function(MTouch, makeMove, "args:x,y,source")
+MTouch::ptr MTouch::makeMove(float x, float y, MTouchSource source) {
+    auto event = MTouch::create();
+    
+    event->mSource = source;
+    event->mStep   = MTouchStep::Move;
+    event->mX      = x;
+    event->mY      = y;
+    
+    return event;
+}
+
+define_reflectable_class_function(MTouch, makeEnd, "args:x,y,source")
+MTouch::ptr MTouch::makeEnd(float x, float y, MTouchSource source) {
+    auto event = MTouch::create();
+    
+    event->mSource = source;
+    event->mStep   = MTouchStep::End;
+    event->mX      = x;
+    event->mY      = y;
+    
+    return event;
+}
+
+define_reflectable_class_function(MTouch, source, "getter")
+define_reflectable_class_function(MTouch, step  , "getter")
+define_reflectable_class_function(MTouch, x     , "getter")
+define_reflectable_class_function(MTouch, y     , "getter")
+
+MTouchSource MTouch::source() { return mSource; }
+MTouchStep   MTouch::step  () { return mStep  ; }
+float        MTouch::x     () { return mX     ; }
+float        MTouch::y     () { return mY     ; }
+
+//mouse move event:
+
+define_reflectable_class_function(MMouseMove, make, "args:x,y")
+MMouseMove::ptr MMouseMove::make(float x, float y) {
+    auto event = MMouseMove::create();
+    
+    event->mX = x;
+    event->mY = y;
+    
+    return event;
+}
+
+define_reflectable_class_function(MMouseMove, x, "getter")
+define_reflectable_class_function(MMouseMove, y, "getter")
+
+float MMouseMove::x() { return mX; }
+float MMouseMove::y() { return mY; }
+
+//mouse wheel event:
+
+define_reflectable_class_function(MMouseWheel, make, "args:delta,x,y")
+MMouseWheel::ptr MMouseWheel::make(float delta, float x, float y) {
+    auto event = MMouseWheel::create();
+    
+    event->mDelta = delta;
+    event->mX     = x    ;
+    event->mY     = y    ;
+    
+    return event;
+}
+
+define_reflectable_class_function(MMouseWheel, delta, "getter")
+define_reflectable_class_function(MMouseWheel, x    , "getter")
+define_reflectable_class_function(MMouseWheel, y    , "getter")
+
+float MMouseWheel::delta() { return mDelta; }
+float MMouseWheel::x    () { return mX    ; }
+float MMouseWheel::y    () { return mY    ; }
+
+//keyboard key event:
+
+//text writing event:
+
+define_reflectable_class_function(MWriting, make, "args:text")
+MWriting::ptr MWriting::make(const std::string &text) {
+    auto event = MWriting::create();
+    event->mText = text;
+    return event;
+}
+
+define_reflectable_class_function(MWriting, text, "getter")
+std::string MWriting::text() {
+    return mText;
+}
+
+//current events:
+
+static dash::lazy<MTouch::ptr     > sTouch     ;
+static dash::lazy<MMouseMove::ptr > sMouseMove ;
+static dash::lazy<MMouseWheel::ptr> sMouseWheel;
+static dash::lazy<MKbKey::ptr     > sKbKey     ;
+static dash::lazy<MWriting::ptr   > sWriting   ;
+
+define_reflectable_function(MResetCurrentTouch     , "args:event")
+define_reflectable_function(MResetCurrentMouseMove , "args:event")
+define_reflectable_function(MResetCurrentMouseWheel, "args:event")
+define_reflectable_function(MResetCurrentKbKey     , "args:event")
+define_reflectable_function(MResetCurrentWriting   , "args:event")
+
+void MResetCurrentTouch     (const MTouch::ptr      &event) { sTouch      = event; }
+void MResetCurrentMouseMove (const MMouseMove::ptr  &event) { sMouseMove  = event; }
+void MResetCurrentMouseWheel(const MMouseWheel::ptr &event) { sMouseWheel = event; }
+void MResetCurrentKbKey     (const MKbKey::ptr      &event) { sKbKey      = event; }
+void MResetCurrentWriting   (const MWriting::ptr    &event) { sWriting    = event; }
+
+define_reflectable_function(MCurrentTouch     )
+define_reflectable_function(MCurrentMouseMove )
+define_reflectable_function(MCurrentMouseWheel)
+define_reflectable_function(MCurrentKbKey     )
+define_reflectable_function(MCurrentWriting   )
+
+MTouch::ptr      MCurrentTouch     () { return sTouch     ; }
+MMouseMove::ptr  MCurrentMouseMove () { return sMouseMove ; }
+MMouseWheel::ptr MCurrentMouseWheel() { return sMouseWheel; }
+MKbKey::ptr      MCurrentKbKey     () { return sKbKey     ; }
+MWriting::ptr    MCurrentWriting   () { return sWriting   ; }
--- appsrc/mevent.h
+++ appsrc/mevent.h
@@ -1,3 +1,142 @@
 #pragma once
 
 #include "mobject.h"
+
+//event:
+
+m_enum(MEvent) {
+    Touch      = 1,
+    MouseMove  = 2,
+    MouseWheel = 3,
+    KbKey      = 4,
+    Writing    = 5,
+};
+
+//touch event:
+
+m_enum(MTouchSource) {
+    Finger  = 1,
+    LButton = 1,
+};
+
+m_enum(MTouchStep) {
+    Begin = 1,
+    Move  = 2,
+    End   = 3,
+};
+
+m_class(MTouch, MObject) {
+public:
+    static MTouch::ptr makeBegin(float x, float y, MTouchSource source);
+    static MTouch::ptr makeMove (float x, float y, MTouchSource source);
+    static MTouch::ptr makeEnd  (float x, float y, MTouchSource source);
+    
+    MTouchSource source();
+    MTouchStep   step  ();
+    float        x     ();
+    float        y     ();
+    
+private:
+    MTouchSource mSource = MTouchSource::Finger;
+    MTouchStep   mStep   = MTouchStep  ::Begin ;
+    float        mX      = 0;
+    float        mY      = 0;
+};
+
+//mouse move event:
+
+m_class(MMouseMove, MObject) {
+public:
+    static MMouseMove::ptr make(float x, float y);
+    
+    float x();
+    float y();
+    
+private:
+    float mX = 0;
+    float mY = 0;
+};
+
+//mouse wheel event:
+
+m_class(MMouseWheel, MObject) {
+public:
+    static MMouseWheel::ptr make(float delta, float x, float y);
+    
+    float delta();
+    float x    ();
+    float y    ();
+
+private:
+    float mDelta = 0;
+    float mX     = 0;
+    float mY     = 0;
+};
+
+//keyboard key event:
+
+typedef int MKbModifiers;
+
+const int MKbModifier_Alt   = 1     ;
+const int MKbModifier_Caps  = 1 << 1;
+const int MKbModifier_Cmd   = 1 << 2;
+const int MKbModifier_Ctrl  = 1 << 3;
+const int MKbModifier_Shift = 1 << 4;
+
+m_enum(MKbKeyCode) {
+    Null  = 0,
+
+    Back  = 0x08,
+    Tab   = 0x09,
+    Enter = 0x0D,
+    Space = 0x20,
+
+    Left  = 0x25,
+    Up    = 0x26,
+    Right = 0x27,
+    Down  = 0x28,
+
+    A     = 'A' ,
+    D     = 'D' ,
+    S     = 'S' ,
+    W     = 'W' ,
+};
+
+m_class(MKbKey, MObject) {
+public:
+    static MKbKey::ptr make(MKbKeyCode code, MKbModifiers modifiers);
+    
+    MKbKeyCode   code     ();
+    MKbModifiers modifiers();
+    char         printed  ();
+    
+private:
+    MKbKeyCode   mCode      = MKbKeyCode::Null;
+    MKbModifiers mModifiers = 0;
+};
+
+//text writing event:
+
+m_class(MWriting, MObject) {
+public:
+    static MWriting::ptr make(const std::string &text);
+    
+    std::string text();
+    
+private:
+    std::string mText;
+};
+
+//current events:
+
+void MResetCurrentTouch     (const MTouch::ptr      &event);
+void MResetCurrentMouseMove (const MMouseMove::ptr  &event);
+void MResetCurrentMouseWheel(const MMouseWheel::ptr &event);
+void MResetCurrentKbKey     (const MKbKey::ptr      &event);
+void MResetCurrentWriting   (const MWriting::ptr    &event);
+
+MTouch::ptr      MCurrentTouch     ();
+MMouseMove::ptr  MCurrentMouseMove ();
+MMouseWheel::ptr MCurrentMouseWheel();
+MKbKey::ptr      MCurrentKbKey     ();
+MWriting::ptr    MCurrentWriting   ();
