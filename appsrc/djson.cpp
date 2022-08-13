12d219445dcdb6598b0d741b26753374414f62f2
wujia <wzsayiie@qq.com>
2022-08-13 18:34:58 +0800

    json library semi-finished product.

--- appsrc/djson.cpp
+++ appsrc/djson.cpp
@@ -2,4 +2,43 @@
 
 namespace dash {
 
+std::string json_object::encode() const {
+    json_encode_context context;
+
+    try {
+        on_encode(&context);
+        return context.buffer;
+
+    } catch (...) {
+        return "";
+    }
+}
+
+void json_object::parse(const std::string &text) {
+}
+
+void json_object::on_encode(void *context) const  {
+    auto ctx = (json_encode_context *)context;
+
+    ctx->buffer.append("{");
+    ctx->buffer.append(json_endline_text);
+
+    for (auto pair = begin(); pair != end(); ++pair) {
+        //delimiter.
+        if (pair != begin()) {
+            ctx->buffer.append(",");
+            ctx->buffer.append(json_endline_text);
+        }
+        //subitem.
+        ctx->indent += 1;
+        pair->second->on_encode(ctx);
+        ctx->indent -= 1;
+    }
+
+    ctx->buffer.append("}");
+}
+
+void json_object::on_parse(void *context) {
+}
+
 } //end dash.
--- appsrc/djson.h
+++ appsrc/djson.h
@@ -4,4 +4,42 @@
 
 namespace dash {
 
+const char *const json_indent_text  = "  ";
+const char *const json_endline_text = "\n";
+
+struct json_encode_context {
+    std::string buffer;
+    int indent = 0;
+};
+
+struct json_parse_context {
+};
+
+template<class Type> class json : public encodable<Type> {
+public:
+    typedef encodable<Type> _inheritable;
+
+    using _inheritable::_inheritable;
+    using _inheritable::operator=;
+
+public:
+    void on_encode(void *context) const override {
+    }
+
+    void on_parse(void *context) override {
+    }
+};
+
+class d_exportable json_object : public encodable_object {
+public:
+    std::string encode() const;
+    void parse(const std::string &text);
+
+public:
+    void on_encode(void *context) const override;
+    void on_parse (void *context)       override;
+};
+
 } //end dash.
+
+#define d_json_object(cls) d_encodable_object(cls)
