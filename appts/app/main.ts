4386bac78b3de0318f2180bfc646ab11b4558316
吴佳 <wzsayiie@qq.com>
2023-01-09 18:27:12 +0800

    code template.

--- appts/app/main.ts
+++ appts/app/main.ts
@@ -1,1 +1,20 @@
-//
+import {
+    CNavigationController,
+    CViewController,
+    CWindow,
+    MWindow,
+} from "./host/native"
+
+class MainViewController extends CViewController {
+
+    onViewLoad(): void {
+    }
+}
+
+(function () {
+    let mainWindow = <CWindow> MWindow.mainWindow
+    let navigator  = <CNavigationController> mainWindow.rootViewController
+
+    let page = new MainViewController()
+    navigator.pushPage(page)
+})()
